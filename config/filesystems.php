f7d162fd7e2c3da6acd0f6c883f984948bbe260f
Romack Natividad <romack@qstrike.com>
2015-07-30 16:36:39 +0800

    Fixed wrong constant var

--- config/filesystems.php
+++ config/filesystems.php
@@ -64,7 +64,7 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key'    => env('AWS_ACCESS_KEY'),
+            'key'    => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_REGION'),
             'bucket' => env('AWS_BUCKET'),
