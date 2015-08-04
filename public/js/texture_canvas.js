5bd18d84f59130a09e227758680bccb201c60514
arthur-v-abogadil <arthur@qstrike.com>
2015-08-04 09:11:29 +0800

    More refactors

--- public/js/texture_canvas.js
+++ public/js/texture_canvas.js
@@ -1,80 +1,52 @@
 		
-		window.texture_canvas.current_image = '';
-
-		function change_color_fabric(obj,color_value){
-
-			selectedObject = window.texture_canvas.objects[obj];
-
-			selectedObject.filters.push(
-    			new fabric.Image.filters.Tint({color:color_value, opacity:1})
-    		);
-
-    		canvas = window.texture_canvas.canvas;
-			selectedObject.applyFilters(canvas.renderAll.bind(canvas));
-
-			setTimeout(function() {
-
-				update_fabric_material();
-
-  			}, 100);
-			
-		}
-
-		function add_blending(obj){
-
-			selectedObject = window.texture_canvas.objects[obj];
-
-			
-			selectedObject.filters.push(
-    			new fabric.Image.filters.Multiply({opacity:0.5})
-    		);
-
-    		canvas = window.texture_canvas.canvas;
-			selectedObject.applyFilters(canvas.renderAll.bind(canvas));
-
-			update_fabric_material();
-
-		}
-
 	$(document).ready(function() {
+
+
+		/// Properties and Vars
 
 		window.texture_canvas = {};
 		window.texture_canvas.texture_style_folder = '/images/materials/baseball_1/';
+		window.texture_canvas['objects'] = {};
+		window.texture_canvas['canvas'] = new fabric.Canvas('texture_canvas');
+		
+		var canvas = window.texture_canvas['canvas'];
 
-		texture_canvas.refresh_model = function refresh_material() {
+		/// End Properties and Vars
+
+
+
+		/// Methods
+
+		texture_canvas.refresh_model = function refresh_model() {
+
+			// THREE.ImageUtils.crossOrigin = '';
 
 			target = 'jersey';
-			// THREE.ImageUtils.crossOrigin = '';
 			
 			var imag = texture_canvas.canvas.toDataURL('image/png');
 			window.texture = THREE.ImageUtils.loadTexture(imag);
+			
 			texture = window.texture;
-
-			texture.needsUpdate = true;
-			//bmap.needsUpdate = true;
-
-			reset_camera();
-
-			//	var texture = THREE.ImageUtils.loadTexture( "/images/materials/material_" + textureImage + ".png" );
-			//	var bmap =  THREE.ImageUtils.loadTexture("/images/materials/material_" + bumpMapImage + "_bump.png", {}, function(){});
-
 			texture.wrapS = THREE.RepeatWrapping;
 			texture.wrapT = THREE.RepeatWrapping;
 			texture.wrapS = THREE.ClampToEdgeWrapping;
 			texture.wrapT = THREE.ClampToEdgeWrapping;
 			texture.minFilter = THREE.LinearFilter;
 			texture.maxFilter = THREE.LinearFilter;
+			texture.repeat.set(1,1);
+			texture.needsUpdate = true;
 
-			window.texture.repeat.set(1,1);
+			reset_camera();
 
+			// var texture = THREE.ImageUtils.loadTexture( "/images/materials/material_" + textureImage + ".png" );
+			// var bmap =  THREE.ImageUtils.loadTexture("/images/materials/material_" + bumpMapImage + "_bump.png", {}, function(){});
 			// bmap.wrapS = THREE.RepeatWrapping;
 			// bmap.wrapT = THREE.RepeatWrapping;
 			// bmap.wrapS = THREE.ClampToEdgeWrapping;
 			// bmap.wrapT = THREE.ClampToEdgeWrapping;
-			//bmap.minFilter = THREE.LinearFilter;
+			// bmap.minFilter = THREE.LinearFilter;
 			// bmap.repeat.set(1,1);
-
-			var texture_color = '0x8c2332';
+			//bmap.needsUpdate = true;
 
 			window.material = new THREE.MeshPhongMaterial({ 
 				texture_color: 0x8c2332, 
@@ -93,9 +65,6 @@
 	        obj.geometry.computeTangents();
 
 	        // move_camera(target);
-
-			// var bmap =  THREE.ImageUtils.loadTexture(bumpMapImage, {}, function(){});
-			//var bmap = new THREE.Texture(texture_image);
 				
 		}
 
@@ -125,20 +94,40 @@
 
 		}
 
-		window.texture_canvas['objects'] = {};
-		window.texture_canvas['canvas'] = new fabric.Canvas('texture_canvas');
-		
-		var canvas = window.texture_canvas['canvas'];
+		texture_canvas.change_color = function (obj,color_value){
+
+			selectedObject = window.texture_canvas.objects[obj];
+
+			selectedObject.filters.push(
+				new fabric.Image.filters.Tint({color:color_value, opacity:1})
+			);
+
+			canvas = window.texture_canvas.canvas;
+			selectedObject.applyFilters(canvas.renderAll.bind(canvas));
+
+			setTimeout(function() {
+
+				texture_canvas.refresh_model();
+
+			}, 100);
+			
+		}
+
+		/// End Methods
+
+
+
+		/// Initial Textures
 		
 		texture_canvas.change_texture('base', 'base.jpg');
 		texture_canvas.change_texture('sleeves', 'sleeves.png');
 		texture_canvas.change_texture('pipings', 'pipings.png');
 
-		canvas.on('mouse:up', function(options) {
-		
-			update_fabric_material();
+		/// End Initial Textures
 
-		});
+
+
+		/// Events
 
 		canvas.on('object:added', function(options) {
 
@@ -155,6 +144,14 @@
 			}
 
 		});
+
+		canvas.on('mouse:up', function(options) {
+		
+			update_fabric_material();
+
+		});
+
+		/// End Events
 
 		
 
