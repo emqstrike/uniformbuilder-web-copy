77eca4ec643ea6a1b0ba1111bcfe360e47263ee8
Romack Natividad <romack@qstrike.com>
2015-08-06 12:52:27 +0800

    Added `UsersController` and users template

---
+++ resources/views/administration/users/user-create.blade.php
@@ -0,0 +1,108 @@
+@extends('administration.main')
+ 
+@section('content')
+
+<div class="container-fluid main-content">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-info">
+                <div class="panel-heading">Add New User</div>
+                <div class="panel-body">
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    <form class="form-horizontal" role="form" method="POST" action="/administration/material/add" enctype="multipart/form-data" id='create-material-form'>
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                        @if (Session::has('flash_message'))
+                        <div class="alert alert-error">{{ Session::get('flash_message') }}</div>
+                        @endif
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">First Name</label>
+                            <div class="col-md-6">
+                                <input type="text" class="form-control user-first-name" name="first_name" value="{{ old('first_name') }}">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Last Name</label>
+                            <div class="col-md-6">
+                                <input type="text" class="form-control user-last-name" name="last_name" value="{{ old('last_name') }}">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Email Address</label>
+                            <div class="col-md-6 material">
+                                <input type="text" class="form-control user-email" name="email">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Password</label>
+                            <div class="col-md-6 bump">
+                                <input type="password" class="form-control password" name="password">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Confirm Password</label>
+                            <div class="col-md-6 shadow">
+                                <input type="password" class="form-control shadow-file" name="confirm_password">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Type</label>
+                            <div class="col-md-6">
+                                <select name='factory_code' class="form-control user-type">
+                                    <option value='normal'>Normal</option>
+                                    <option value='dealer'>Dealer</option>
+                                    <option value='administrator'>Administrator</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary create-material" style="margin-right: 15px;" disabled="disabled">
+                                    Add New User
+                                </button>
+                                <a href="/administration/materials" class="btn btn-danger" style="margin-right: 15px;">
+                                    Cancel
+                                </a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@section('custom-scripts')
+
+$(document).ready(function(){
+    $('#create-material-form').submit(function(){
+        $('.flash-alert .flash-progress').show();
+        $('.flash-alert .flash-title').text('Creating New Material');
+        $('.flash-alert .flash-sub-title').text('Uploading');
+        $('.flash-alert .flash-message').text('Please wait while we are uploading the images...');
+        $('.flash-alert').addClass('alert-info');
+        $('.flash-alert').show();
+        $('.main-content').fadeOut('slow');
+    });
+});
+
+@endsection
---
+++ resources/views/administration/users/users.blade.php
@@ -0,0 +1,155 @@
+@extends('administration.main')
+ 
+@section('content')
+
+@if (Session::has('message'))
+<div class="alert alert-info alert-dismissable flash-alert">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+        ×
+    </button>
+    <h4 class='flash-title'>Alert</h4>
+    <strong class='flash-sub-title'></strong> <span class='flash-message'>{{ Session::get('message') }}</span>
+</div>
+@endif
+
+<div class="col-md-12">
+    <h1>
+        <span class="glyphicon glyphicon-user"></span>
+        Users
+        <small>
+            <a href="/administration/user/add" class='btn btn-xs btn-success'>
+                <span class="glyphicon glyphicon-plus-sign"></span>
+                Add New User
+            </a>
+        </small>
+    </h1>
+</div>
+
+<div class="row-fluid col-md-9">
+    <table class='table table-bordered users-table display' cellspacing="0">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Account Type</th>
+                <th>Email</th>
+                <th>Active Status</th>
+                <th></th>
+            </tr>
+        </thead>
+
+        @forelse ($users as $user)
+
+        <tbody>
+            <tr class='user-{{ $user->id }} {{ (!$user->active) ? ' inactive' : '' }}'>
+                <td>
+                    {{ $user->first_name }} {{ $user->last_name }}
+                </td>
+                <td>
+                    <span class="label label-{{ ($user->type == 'administrator') ? 'danger' : 'info' }}">{{ ucfirst($user->type) }}</span>
+                </td>
+                <td>
+                    <span class="label label-primary">{{ $user->email }}</span>
+                </td>
+                <td>
+                    <a href="#" class="btn btn-default btn-xs disable-user" data-user-id="{{ $user->id }}" role="button" {{ ($user->active) ? : 'disabled="disabled"' }}>
+                        <i class="glyphicon glyphicon-eye-close"></i>
+                        Disable
+                    </a>
+                    <a href="#" class="btn btn-info btn-xs enable-user" data-user-id="{{ $user->id }}" role="button" {{ ($user->active) ? 'disabled="disabled"' : '' }}>
+                        <i class="glyphicon glyphicon-eye-open"></i>
+                        Enable
+                    </a>
+                </td>
+                <td>
+                    <a href="#" class="btn btn-warning btn-xs view-user" data-user-id="{{ $user->id }}" role="button">
+                        <li class="glyphicon glyphicon-info-sign"></li>
+                        View
+                    </a>
+                    <a href="/administration/user/edit/{{ $user->id }}" class="btn btn-primary btn-xs edit-user" data-user-id="{{ $user->id }}" role="button">
+                        <i class="glyphicon glyphicon-edit"></i>
+                        Edit
+                    </a>
+                    <a href="#" class="btn btn-danger pull-right btn-xs delete-user" data-user-id="{{ $user->id }}" role="button">
+                        <i class="glyphicon glyphicon-trash"></i>
+                        Remove
+                    </a>
+                </td>
+            </tr>
+        </tbody>
+
+        @empty
+
+            <tr>
+                <td colspan='4'>
+                    No Users
+                </td>
+            </tr>
+
+        @endforelse
+    </table>
+</div>
+
+@include('partials.confirmation-modal')
+
+@endsection
+
+@section('custom-scripts')
+
+$(document).ready(function(){
+
+    $('.enable-user').on('click', function(){
+        var id = $(this).data('user-id');
+        var url = "//{{ $api_host }}/api/user/" + id + "/enable/";
+        $.getJSON(url, function(response){
+            if (response.success) {
+                var elem = '.user-' + id;
+                $('.flash-alert .flash-title').text(response.message);
+                $('.flash-alert').addClass('alert-info').fadeIn();
+                $(elem + ' .disable-user').removeAttr('disabled');
+                $(elem + ' .enable-user').attr('disabled', 'disabled');
+                $(elem).removeClass('inactive');
+            }
+        });
+    });
+
+    $('.disable-user').on('click', function(){
+        var id = $(this).data('user-id');
+        var url = "//{{ $api_host }}/api/user/" + id + "/disable/";
+        $.getJSON(url, function(response){
+            if (response.success) {
+                var elem = '.user-' + id;
+                $('.flash-alert .flash-title').text(response.message);
+                $('.flash-alert').addClass('alert-info').fadeIn();
+                $(elem + ' .enable-user').removeAttr('disabled');
+                $(elem + ' .disable-user').attr('disabled', 'disabled');
+                $(elem).addClass('inactive');
+            }
+        });
+    });
+
+    $('.delete-user').on('click', function(){
+        var id = $(this).data('user-id');
+        modalConfirm('Remove user', 'Are you sure you want to delete the user?', id);
+    });
+
+    $('#confirmation-modal .confirm-yes').on('click', function(){
+        var id = $(this).data('value');
+        var url = "//{{ $api_host }}/api/user/" + id + "/delete/";
+        $.getJSON(url, function(response){
+            if (response.success) {
+                $('#confirmation-modal').modal('hide');
+                $('.user-' + id).fadeOut();
+            }
+        });
+    });
+
+    function modalConfirm(title, message, value)
+    {
+        $('#confirmation-modal .modal-title').text(title);
+        $('#confirmation-modal .modal-body').text(message);
+        $('#confirmation-modal .confirm-yes').data('value', value);
+        $('#confirmation-modal').modal('show');
+    }
+});
+
+@endsection
