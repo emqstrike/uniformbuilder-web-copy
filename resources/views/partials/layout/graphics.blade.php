00a289b2241cb342beefe2769e01a9ef7db7b15c
Jester Taguibao <jester@qstrike.com>
2015-08-13 13:42:18 +0800

    added files

---
+++ resources/views/partials/layout/graphics.blade.php
@@ -0,0 +1,9 @@
+<h2>Logo</h2>
+		  <div id="miaDropzone" class="panel-body text-center">					 
+		  	Drop logo here ...					 
+		  </div>				
+		<br />
+		<em>Please upload a transparent PNG
+		<br /> (300px x 300px)</em>
+		<br />
+		<hr />	
---
+++ resources/views/partials/layout/number.blade.php
@@ -0,0 +1,3 @@
+<h2>Numbers</h2>
+<input type="text" name="txtNumber" id="txtNumber" class="txtNumber" maxlength="2" /> <button onclick="texture_canvas.load_number()">Apply</button>		
+<br>
