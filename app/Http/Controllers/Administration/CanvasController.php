4da5932976cd5171b7b92fcf6e9eb440a1403d68
arthur-v-abogadil <arthur@qstrike.com>
2015-08-17 12:46:39 +0800

    Commented Code Cleanup and read asset version from .env file

--- app/Http/Controllers/Administration/CanvasController.php
+++ app/Http/Controllers/Administration/CanvasController.php
@@ -25,33 +25,11 @@ class CanvasController extends Controller
 
         $title = 'Prolook Canvas';
 
-        // For asset versioning, to force update scripts and stylesheets instead of being loaded from browser cache
-        $asset = [
-            'version' => '?v=0.001',
-            'storage' => ''
-        ];
-
-        //// API SAMPLE
-
-        // $response = $this->client->get('categories');
-        // $result = $this->decoder->decode($response->getBody());
-
-        // if ($result->success)
-        // {
-        //     $categories = $result->categories;
-        // }
-        
-        // $categories = new \App\APIClients\UniformCategoriesAPIClient();
-        // $result = $categories->getUniformCategories();
-        // dd($result);
-
-        //// END API SAMPLE
-
-         return view('administration.canvas.index', [
+        return view('administration.canvas.index', [
             
             'page_title' => $title,
-            'asset_version' => $asset['version'],
-            'asset_storage' => $asset['storage'],
+            'asset_version' =>  env('ASSET_VERSION'),
+            'asset_storage' => env('ASSET_STORAGE'),
             
         ]);
 
