254d5a59db0fda53f9677db9e315eedd3b930452
Jethro <jethro@qstrike.com>
2015-08-27 09:38:21 +0800

    updated script source

--- resources/views/administration/users/account_settings.blade.php
+++ resources/views/administration/users/account_settings.blade.php
@@ -89,6 +89,7 @@
 @section('scripts')
 
 <script type="text/javascript" src="/js/libs/spectrum/spectrum.js"></script>
+<script type="text/javascript" src="/js/administration/common.js"></script>
 
 @endsection
 
