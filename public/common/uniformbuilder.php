145fb75a3234d06dab81dbee58917c034eb7c0d4
arthur-v-abogadil <arthur@qstrike.com>
2015-09-01 07:50:06 +0800

    Clean ups

--- public/common/uniformbuilder.php
+++ public/common/uniformbuilder.php
@@ -1,19 +1,7 @@
 <div class="container"> 
-				<!-- <button class="btn btn-default">Jersey</button>
-				<button class="btn btn-default">Pants</button>
-				<button class="btn btn-default">All</button> -->
-<div id="mycanvas" class="mycanvas">
-  <!--   <div class="camera_buttons">
 
-		<button id="btn_free_form" class="btn-white btn btn-default btn-sm" onclick="reset_camera();toggle_free_rotate()"><i class="fa fa-refresh"></i></button>	
-		<button class="btn-white btn btn-default btn-sm" onclick="reset_camera()"><i class="fa fa-history"></i></button>	
+	<div id="mycanvas" class="mycanvas">
 
-		<button class="btn btn-default btn-sm" id="play_left"><i class="fa fa-chevron-left"></i><i class="fa fa-chevron-left"></i></button>
-		<button class="btn btn-default btn-sm" id="rotate_left"><i class="fa fa-chevron-left"></i></button>
-		<button class="btn btn-default btn-sm" id="rotate_right"><i class="fa fa-chevron-right"></i></button>
-		<button class="btn btn-default btn-sm" id="play_right"><i class="fa fa-chevron-right"></i><i class="fa fa-chevron-right"></i></button>
+	</div>
 
-	</div> -->
-
-</div>
 </div>
