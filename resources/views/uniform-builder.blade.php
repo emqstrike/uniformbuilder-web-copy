f5fb14f4c73628b249b1a229dd24a90706765abc
arthur-v-abogadil <arthur@qstrike.com>
2015-09-29 11:08:38 +0800

    Space

--- resources/views/uniform-builder.blade.php
+++ resources/views/uniform-builder.blade.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html>
 
-    <head>
+<head>
 
-        <meta http-equiv="Access-Control-Allow-Origin" content="*"/>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="Uniform Builder">
-        <meta name="author" content="Engineering">
-        <title>{{ $page_title }}</title>
-        <link rel="icon" type="image/png" href="/images/branding/brand.png" />
-        <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href="{{$asset_storage}}/bootstrap/css/bootstrap.min.css{{$asset_version}}">
-        <link rel="stylesheet" href="{{$asset_storage}}/bootstrap/css/bootstrap-theme.min.css{{$asset_version}}">
-        <link rel="stylesheet" href="{{$asset_storage}}/font-awesome/css/font-awesome.min.css{{$asset_version}}">
-        <link rel="stylesheet" href="{{$asset_storage}}/jquery-ui/jquery-ui.min.css{{$asset_version}}">
-        <link rel="stylesheet" href="{{$asset_storage}}/uniform-builder/css/uniform-builder.css{{$asset_version}}">
+<meta http-equiv="Access-Control-Allow-Origin" content="*"/>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Uniform Builder">
+<meta name="author" content="Engineering">
+<title>{{ $page_title }}</title>
+<link rel="icon" type="image/png" href="/images/branding/brand.png" />
+<link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="{{$asset_storage}}/bootstrap/css/bootstrap.min.css{{$asset_version}}">
+<link rel="stylesheet" href="{{$asset_storage}}/bootstrap/css/bootstrap-theme.min.css{{$asset_version}}">
+<link rel="stylesheet" href="{{$asset_storage}}/font-awesome/css/font-awesome.min.css{{$asset_version}}">
+<link rel="stylesheet" href="{{$asset_storage}}/jquery-ui/jquery-ui.min.css{{$asset_version}}">
+<link rel="stylesheet" href="{{$asset_storage}}/uniform-builder/css/uniform-builder.css{{$asset_version}}">
 
-    </head>
+</head>
 
 <body>
 <nav class="navbar navbar-default navbar-fixed-top">
@@ -106,43 +106,43 @@
 
 <!-- Third Party Scripts -->
 
-            <script src="{{$asset_storage}}/jquery/jquery-1.11.3.min.js{{$asset_version}}"></script>
-            <script src="{{$asset_storage}}/jquery-ui/jquery-ui.min.js{{$asset_version}}"></script>
-            <script src="{{$asset_storage}}/underscore/underscore-min.js{{$asset_version}}"></script>
-            <script src="{{$asset_storage}}/bootstrap/js/bootstrap.min.js{{$asset_version}}"></script>
-            <script src="{{$asset_storage}}/fabricjs/fabric.min.js{{$asset_version}}"></script>
-            <script src="{{$asset_storage}}/dropzone/dropzone.js{{$asset_version}}"></script>
-            <script src="{{$asset_storage}}/tether/js/tether.js{{$asset_version}}"></script>
+<script src="{{$asset_storage}}/jquery/jquery-1.11.3.min.js{{$asset_version}}"></script>
+<script src="{{$asset_storage}}/jquery-ui/jquery-ui.min.js{{$asset_version}}"></script>
+<script src="{{$asset_storage}}/underscore/underscore-min.js{{$asset_version}}"></script>
+<script src="{{$asset_storage}}/bootstrap/js/bootstrap.min.js{{$asset_version}}"></script>
+<script src="{{$asset_storage}}/fabricjs/fabric.min.js{{$asset_version}}"></script>
+<script src="{{$asset_storage}}/dropzone/dropzone.js{{$asset_version}}"></script>
+<script src="{{$asset_storage}}/tether/js/tether.js{{$asset_version}}"></script>
 
-            <script src="{{$asset_storage}}/pixi/pixi.js{{$asset_version}}"></script>
+<script src="{{$asset_storage}}/pixi/pixi.js{{$asset_version}}"></script>
 
 <!-- End Third Party Scripts -->
 
 <!-- Uniform Builder Scripts -->
 
-            <script type="text/javascript">
+<script type="text/javascript">
 
-                $( document ).ready( function () {
-                    window.ub = {};
-                    window.ub.objects = {};
-                    window.ub.config = {
-                        api_host: "http://{{ env('API_HOST') }}",
-                        material_id: {{ $material_id }},
-                        category_id: {{ $category_id }},
-                        host: 'http://{{ Request::server ("HTTP_HOST") }}',
-                        thumbnails_path: "{{ env('S3_PATH') }}" + 'thumbnails/'
-                    }
-                @if (Session::has('message'))
-                    setTimeout(function(){
-                        $('.flash-alert').fadeOut();
-                    }, 3000);
-                @endif
-                });
+$( document ).ready( function () {
+    window.ub = {};
+    window.ub.objects = {};
+    window.ub.config = {
+        api_host: "http://{{ env('API_HOST') }}",
+        material_id: {{ $material_id }},
+        category_id: {{ $category_id }},
+        host: 'http://{{ Request::server ("HTTP_HOST") }}',
+        thumbnails_path: "{{ env('S3_PATH') }}" + 'thumbnails/'
+    }
+@if (Session::has('message'))
+    setTimeout(function(){
+        $('.flash-alert').fadeOut();
+    }, 3000);
+@endif
+});
 
-            </script>
+</script>
 
-            <script src="{{$asset_storage}}/uniform-builder/js/utilities.js{{$asset_version}}"></script>
-            <script src="{{$asset_storage}}/uniform-builder/js/uniform-builder.js{{$asset_version}}"></script>
+<script src="{{$asset_storage}}/uniform-builder/js/utilities.js{{$asset_version}}"></script>
+<script src="{{$asset_storage}}/uniform-builder/js/uniform-builder.js{{$asset_version}}"></script>
 
 <!-- End Uniform Builder Scripts -->
 </body>
