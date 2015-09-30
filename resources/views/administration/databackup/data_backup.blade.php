5affe48be3ea32d6199dd3df1f8b2198777f44e1
jestertaguibao <taguibaojester@gmail.com>
2015-09-30 13:24:43 +0800

    layout dbbackup

--- resources/views/administration/databackup/data_backup.blade.php
+++ resources/views/administration/databackup/data_backup.blade.php
@@ -13,9 +13,58 @@
 @endif
 
 <div class="col-md-12">
+    
+    <button type="button" class="btn btn-default btn-lg">
+        <span class="glyphicon glyphicon-export" aria-hidden="true"></span> Backup Database
+    </button>
+
+<table class="table table-hover">      
+<div class="panel-heading">
+    <tr>
+      <td class="active">
+       DATE:
+      </td>
+      <td class="active">
+        LOCATION
+      </td>
+      <td class="active">
+        RESTORE
+      </td>
+    </tr>
+</div>
+    
+    <tr>
+      <td class="active">
+        08/26/1992 12:00pm
+      </td>
+      <td class="active">
+        Alpha Server
+      </td>
+      <td class="active">
+        <button type="button" class="btn btn-default btn-lg">
+          <span class="glyphicon glyphicon-import" aria-hidden="true"></span> Restore Database
+        </button>
+      </td>
+    </tr>
+
+    <tr>
+      <td class="active">
+        08/25/1992 12:00pm
+      </td>
+      <td class="active">
+        Alpha Server
+      </td>
+      <td class="active">
+        <button type="button" class="btn btn-default btn-lg">
+          <span class="glyphicon glyphicon-import" aria-hidden="true"></span> Restore Database
+        </button>
+      </td>
+    </tr>
+</table>
 
 
-TEST
+ 
+
 </div>
 
 @include('partials.confirmation-modal')
