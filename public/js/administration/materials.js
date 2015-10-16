1aaadbe80e304147feb4c6c9b70e5a2c9bc0221e
Jethro <jethro@qstrike.com>
2015-10-16 15:13:20 +0800

    applied changes

--- public/js/administration/materials.js
+++ public/js/administration/materials.js
@@ -200,7 +200,6 @@ $(document).ready(function() {
     });
 
     $('.add-material-option').on('click', function(){
-        $('#form-action').attr('value','add');
         var material = {
             id: $(this).data('material-id'),
             name: $(this).data('material-name')
@@ -222,7 +221,6 @@ $(document).ready(function() {
     });
 
     $('.edit-material-option').on('click', function(){
-        $('#material-option-form').attr('action','/administration/material_option/save');
         var material = {
             id: $(this).data('material-id'),
             name: $(this).data('material-name'),
