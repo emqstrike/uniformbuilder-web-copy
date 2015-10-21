528d45c5ecba4a8ab8ac229f39dd5f986f741679
Romack Natividad <romack@qstrike.com>
2015-10-21 18:02:20 +0800

    The Base64-encoded-image saved to file Utility method `FileUtility::saveBase64Image()`

--- app/Utilities/FileUtility.php
+++ app/Utilities/FileUtility.php
@@ -14,4 +14,23 @@ class FileUtility
         $filename = substr($filePath, (strrpos($filePath, '/') + 1), (strlen($filePath) - strrpos($filePath, '/')));
         return $filename;
     }
+
+    /**
+     * Exports a base64-encoded email to a PNG file
+     * @param String a base64-encoded string
+     * @return Mixed False when decoding failed; File path whenever decoding worked
+     */
+    public static function saveBase64Image($data)
+    {
+        // This lines extracts the base64 encoded data from the string
+        list($type, $data) = explode(';', $data);
+        list($tmp, $data) = explode(',', $data);
+
+        $filePath = '/tmp/' . Random::randomize(16) . '.png';
+        if (file_put_contents($filePath, base64_decode($data)) === false)
+        {
+            return false;
+        }
+        return $filePath;
+    }
 }
