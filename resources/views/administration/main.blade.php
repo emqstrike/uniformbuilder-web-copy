afdee9fb2df4cf036c84d7c57f07d0bdc24f3840
Romack Natividad <romack@qstrike.com>
2015-10-28 09:13:27 +0800

    Use enviroment configuration settings

--- resources/views/administration/main.blade.php
+++ resources/views/administration/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>QuickStrike Uniform Builder Administration</title>
+    <title>Administration | {{ env('WEBSITE_NAME') }}</title>
 
     <link href="/css/app.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="/bootstrap/css/bootstrap.min.css">
