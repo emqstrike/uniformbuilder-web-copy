22caad10f698576d3fc12f4b7573838cb5b28732
Arthur Abogadil <arthur@qstrike.com>
2016-02-10 15:38:36 +0800

    Correction to creation of node in transform applications

--- public/uniform-builder/js/uniform-builder-applications.js
+++ public/uniform-builder/js/uniform-builder-applications.js
@@ -1310,9 +1310,6 @@ $(document).ready(function() {
 
     ub.data.applications_transformed = {};
 
-    ub.data.applications_distinct_names = {};
-    ub.data.temp_arr = [];
-
     ub.funcs.transformedApplications = function () {
 
         var material_options = ub.current_material.materials_options;
@@ -1321,14 +1318,17 @@ $(document).ready(function() {
 
         _.each(shapes, function(shape){
 
-            var app_properties = JSON.parse(shape.applications_properties);
-            app_properties = JSON.parse(app_properties);
-
+            var app_properties = JSON.parse(shape.applications_properties.slice(1, -1));
+            
             if(app_properties !== null){
                 
-                apps_transformed[shape.name] = {};
-
                 _.each(app_properties, function(obj){
+
+                    if (typeof apps_transformed[shape.name] === "undefined") {
+                
+                        apps_transformed[shape.name] = {};    
+                
+                    }
 
                     if (typeof apps_transformed[shape.name][obj.id] === 'undefined'){
                         
