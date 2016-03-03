a19fa5bfd1394a7f9d8369da2bf09ef828497460
Arthur Abogadil <arthur@qstrike.com>
2016-03-03 13:39:56 +0800

    Color Code on Picker, etc... Restore Default Responsiveness

--- resources/views/uniform-builder.blade.php
+++ resources/views/uniform-builder.blade.php
@@ -4,6 +4,7 @@
 <meta http-equiv="Access-Control-Allow-Origin" content="*"/>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="Uniform Builder">
 <meta name="author" content="Engineering">
 <meta name="csrf-token" content="{{ csrf_token() }}" />
