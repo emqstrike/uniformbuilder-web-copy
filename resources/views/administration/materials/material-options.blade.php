7d516bd807962192eb4f81842be7d97fed60320c
aljim <aljim0990@gmail.com>
2016-06-15 11:23:59 -0500

    Merged accents to materials

--- resources/views/administration/materials/material-options.blade.php
+++ resources/views/administration/materials/material-options.blade.php
@@ -447,10 +447,11 @@
 <script type="text/javascript" src="/js/libs/bootstrap-table/bootstrap-table.min.js"></script>
 <script type="text/javascript" src="/js/libs/select2/select2.min.js"></script>
 <script type="text/javascript" src="/js/administration/common.js"></script>
-<script type="text/javascript" src="/js/administration/materials.js"></script>
+
 <script type="text/javascript" src="/fabricjs/fabric.min.js"></script>
 <script type="text/javascript" src="/jquery-ui/jquery-ui.min.js"></script>
 <script type="text/javascript" src="/js/ddslick.min.js"></script>
+<script type="text/javascript" src="/js/administration/materials.js"></script>
 @if (Session::has('message'))
 <script type="text/javascript">
 $(document).ready(function(){
