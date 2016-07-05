e3da9854fc338c00cbf98fbad18d24304e7b909a
Arthur Abogadil <arthur@qstrike.com>
2016-07-05 08:08:15 +0800

    fix Chatbox Test

--- resources/views/uniform-builder.blade.php
+++ resources/views/uniform-builder.blade.php
@@ -345,4 +345,15 @@
 
 </script>
 
+<script type="text/javascript">
+window.__lc = window.__lc || {};
+window.__lc.license = 7737151;
+(function() {
+ var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
+ lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
+ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
+})();
+</script>
+
+
 </html>
