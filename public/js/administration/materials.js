92d949a96c729a8e322c907800e6a33b3db6f996
aljim <aljim0990@gmail.com>
2016-08-04 08:59:54 +0800

    fix bugs on materials modal

--- public/js/administration/materials.js
+++ public/js/administration/materials.js
@@ -382,6 +382,15 @@ $(document).ready(function() {
     function syncMOLayers(){
         var length = $('.options-row').length;
         $(".options-row").each(function(i) {
+            console.log(i);
+            if(0<i){
+                $(this).find(".remove-row").show();
+
+            }else{
+                 $(this).find(".remove-row").hide();
+            }
+
+
             // $(this).find(".layer-number").text(length);
             // $(this).find(".layer-number").val(length);
             $(this).find(".mo-layer").val(length);
@@ -953,7 +962,10 @@ $(document).ready(function() {
     });
 
     $('.add-multiple-material-option').on('click', function(){
+
         $('#add-multiple-options-modal').modal('show');
+
+
     });
 
     $('.cleanup-material').on('click', function(){
@@ -3437,7 +3449,15 @@ function accentMascotSelect(data,accentMascot,rowIndex){
     });
 
     
+ $(document).on('click', '.remove-row', function() {
+console.log("index");
+console.log($(".remove-row").index(this));
 
+
+    $(".options-row").eq($(".remove-row").index(this)).remove();
+    
+
+});
       $(document).on('change', '.mascotFilter', function() {
 
                   
@@ -3472,7 +3492,7 @@ function accentMascotSelect(data,accentMascot,rowIndex){
         });
         
     function bringingPointToFront(){
-        console.log("world");
+       
          canvas.forEachObject(function(key,obj){  
           
             var subjectName = ""+ canvas.item(obj).name +"";
