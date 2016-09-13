852f8dbbbb44d66dda2d24327ef00012f21d430b
Arthur Abogadil <arthur@qstrike.com>
2016-09-13 11:33:57 +0800

    History UI

--- public/uniform-builder/js/uniform-builder.js
+++ public/uniform-builder/js/uniform-builder.js
@@ -139,7 +139,7 @@ $(document).ready(function () {
             }
 
             ub.data.undoHistory = [];
-            initUndo();
+            ub.funcs.initUndo();
             
         };
 
@@ -4822,6 +4822,7 @@ $(document).ready(function () {
         $('.main_viewport').hide();
 
         $('div#main-row').hide();
+        $('span.undo-btn').hide();
         $('div.special_modifiers').hide();
         $('div#main-picker-container').show();
 
@@ -5454,133 +5455,7 @@ $(document).ready(function () {
 
     /// End Forgot Password
     
-    /// Undo 
-
-    ub.funcs.undoHandler = function (e) {
-      
-      var evtobj = window.event? event : e
-
-      if (evtobj.keyCode == 90 && (evtobj.ctrlKey || evtobj.metaKey)) {
-        
-           ub.funcs.undo();
-
-      }
-
-    };
-
-    ub.funcs.undo = function () {
-
-        var _size = _.size(ub.data.undoHistory);
-
-        if (_size === 0) { console.log('Undo Stack is empty'); return; }
-
-        var _historyItem = ub.data.undoHistory.pop();
-
-        switch (_historyItem.operationType) {
-
-            case "color change": 
-
-                if (_historyItem.objectType === "material option") {
-
-                    var _partName = _historyItem.settingsObject.code.replace('left_', '').replace('right_', '').toTitleCase();
-                    ub.funcs.ui.setMaterialOptionColor(_partName, _historyItem.oldValue, 'from undo');
-
-                    var _index = ub.funcs.getIndexByName(_historyItem.settingsObject.code);
-                    ub.funcs.activatePartByIndex(_index);
-                    ub.funcs.resetHighlights();
-
-                }
-
-                if (_historyItem.objectType === "application") {
-
-                    var _oldValue = _historyItem.oldValue;
-                    ub.funcs.activateApplications(_oldValue.applicationCode);
-
-                    var _colorCode = _oldValue.color.color_code;
-                    var _layerNo = _oldValue.layerNo;
-
-                    $('span.colorItem[data-layer-no="' + _layerNo + '"][data-color-code="' + _colorCode + '"]').data('temp','undo').click();
-
-                }
-
-                break;
-
-            case "position, scale, rotation change":
-
-                if (_historyItem.objectType === "application") { 
-
-                    var _oldValue = _historyItem.oldValue;
-                    var _settingsObject = ub.funcs.getSettingsObject(_historyItem.settingsObject.code);
-
-                    _.each (_oldValue, function (val) {
-
-                        var application_obj = ub.objects[val.perspective + "_view"]['objects_' + val.applicationCode];
-
-                        if (typeof application_obj.oldPositionX !== "undefined") {
-
-                            view = _.find(_settingsObject.application.views, {perspective: val.perspective });
-
-                            if (typeof view.application.scale === "undefined") {
-
-                                view.application.scale = {x: 1, y: 1};
-
-                            }
-
-                            view.application.scale.x = application_obj.scale.x = val.scale.x;
-                            view.application.scale.y = application_obj.scale.y = val.scale.y;
-                            
-                            view.application.center.x = application_obj.position.x = val.position.x;
-                            view.application.center.y = application_obj.position.y = val.position.y;
-                            
-                            view.application.rotation = application_obj.rotation = val.rotation;  
-
-                        }
-
-                    });
-
-                    ub.funcs.activateMoveTool(_historyItem.settingsObject.code);
-
-                }
-
-                break;
-
-            default: 
-
-                console.error('Unhandled operation type: ' + _historyItem.operationType + ' '  +  _historyItem.objectType);
-
-        }
-
-    }
-
-    document.onkeydown = ub.funcs.undoHandler;
-
-    function initUndo() {
-
-        ub.funcs.pushOldState = function (operationType, objectType, settingsObject, oldValue, newValue) {
-
-        // if (ub.current_material.material.uniform_category !== "Football") {
-
-            var _undoData = {
-
-                operationType: operationType,
-                objectType: objectType,
-                settingsObject: settingsObject,
-                oldValue: oldValue, 
-                newValue: newValue,
-
-            }
-
-            ub.data.undoHistory.push (_undoData);
-
-        // }
-        
-        }
-
-    }
-
-    /// End Undo
-
-
+    
     /// Saving, Loading and Sharing /// 
 
     // New Design
