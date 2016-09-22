902bc93a364573204ad5111fe85e65d138e67ee0
Arthur Abogadil <arthur@qstrike.com>
2016-09-22 14:46:15 +0800

    Update Dragged Captions, Trap Undefined, Add instructions to draggable

--- resources/views/partials/panels/layers.blade.php
+++ resources/views/partials/panels/layers.blade.php
@@ -1,6 +1,7 @@
 <div id = "layers-order">
     
-    <span class="layers-header">Layer Order</span>    
+    <span class="layers-header">Layer Order</span>    .
+    <em>Drag the layers to adjust which object is below or above another object</em>
 
     <div class="layers-container" id="layersContainer">
 
