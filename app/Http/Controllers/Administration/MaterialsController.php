adf531041b2bfbbe05f0cc16c3f6553b06c8590f
jethqx <jethro@qstrike.com>
2016-12-06 16:53:07 +0800

    Removed commented structured data in materialscontroller

--- app/Http/Controllers/Administration/MaterialsController.php
+++ app/Http/Controllers/Administration/MaterialsController.php
@@ -808,58 +808,7 @@ class MaterialsController extends Controller
         {
             $message = $e->getMessage();
             dd($message);
-            // return Redirect::to('/administration/materials')
-            //                 ->with('message', 'There was a problem uploading your files');
         }
-
-        // try {
-            // $structured_data['1/8'] = [];
-            // $structured_data['1/8']['front_pos_1'] = $data['f_position_1_18'];
-            // $structured_data['1/8']['back_pos_1'] = $data['b_position_1_18'];
-            // $structured_data['1/8']['left_pos_1'] = $data['l_position_1_18'];
-            // $structured_data['1/8']['right_pos_1'] = $data['r_position_1_18'];
-            // $structured_data['1/8']['front_pos_2'] = $data['f_position_2_18'];
-            // $structured_data['1/8']['back_pos_2'] = $data['b_position_2_18'];
-            // $structured_data['1/8']['left_pos_2'] = $data['l_position_2_18'];
-            // $structured_data['1/8']['right_pos_2'] = $data['r_position_2_18'];
-            // $structured_data['1/8']['front_pos_3'] = $data['f_position_3_18'];
-            // $structured_data['1/8']['back_pos_3'] = $data['b_position_3_18'];
-            // $structured_data['1/8']['left_pos_3'] = $data['l_position_3_18'];
-            // $structured_data['1/8']['right_pos_3'] = $data['r_position_3_18'];
-
-            // $structured_data['1/4'] = [];
-            // $structured_data['1/4']['front_pos_1'] = $data['_position_1_14'];
-            // $structured_data['1/4']['back_pos_1'] = $data['b_position_1_14'];
-            // $structured_data['1/4']['left_pos_1'] = $data['l_position_1_14'];
-            // $structured_data['1/4']['right_pos_1'] = $data['r_position_1_14'];
-            // $structured_data['1/4']['front_pos_2'] = $data['f_position_2_14'];
-            // $structured_data['1/4']['back_pos_2'] = $data['b_position_2_14'];
-            // $structured_data['1/4']['left_pos_2'] = $data['l_position_2_14'];
-            // $structured_data['1/4']['right_pos_2'] = $data['r_position_2_14'];
-            // $structured_data['1/4']['front_pos_3'] = $data['f_position_3_14'];
-            // $structured_data['1/4']['back_pos_3'] = $data['b_position_3_14'];
-            // $structured_data['1/4']['left_pos_3'] = $data['l_position_3_14'];
-            // $structured_data['1/4']['right_pos_3'] = $data['r_position_3_14'];
-
-            // $structured_data['1/2'] = [];
-            // $structured_data['1/2']['front_pos_1'] = $data['f_position_1_12'];
-            // $structured_data['1/2']['back_pos_1'] = $data['b_position_1_12'];
-            // $structured_data['1/2']['left_pos_1'] = $data['l_position_1_12'];
-            // $structured_data['1/2']['right_pos_1'] = $data['r_position_1_12'];
-            // $structured_data['1/2']['front_pos_2'] = $data['f_position_2_12'];
-            // $structured_data['1/2']['back_pos_2'] = $data['b_position_2_12'];
-            // $structured_data['1/2']['left_pos_2'] = $data['l_position_2_12'];
-            // $structured_data['1/2']['right_pos_2'] = $data['r_position_2_12'];
-            // $structured_data['1/2']['front_pos_3'] = $data['f_position_3_12'];
-            // $structured_data['1/2']['back_pos_3'] = $data['b_position_3_12'];
-            // $structured_data['1/2']['left_pos_3'] = $data['l_position_3_12'];
-            // $structured_data['1/2']['right_pos_3'] = $data['r_position_3_12'];
-        // }
-        // catch (S3Exception $e)
-        // {
-        //     $message = $e->getMessage();
-        //     dd($message);
-        // }
 
         dd($structured_data);
     }
