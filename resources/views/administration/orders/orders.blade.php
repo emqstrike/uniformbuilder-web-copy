e9805c457cf39e54dd5a7b9ceb264ccb1a2c1494
jethqx <jethro@qstrike.com>
2016-12-27 15:21:45 +0800

    Orders page in Datatables

--- resources/views/administration/orders/orders.blade.php
+++ resources/views/administration/orders/orders.blade.php
@@ -3,6 +3,7 @@
 @section('styles')
 <link rel="stylesheet" type="text/css" href="/css/libs/bootstrap-table/bootstrap-table.min.css">
 <link rel="stylesheet" type="text/css" href="/css/custom.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs-3.3.7/jqc-1.12.4/dt-1.10.13/af-2.1.3/b-1.2.4/b-colvis-1.2.4/r-2.1.0/datatables.min.css"/>
 @endsection
 
 @section('content')
@@ -39,9 +40,6 @@
                                 {{ $order->id }}
                             </td>
                             <td>
-                                <!-- <img src="{{ $order->upper_front_thumbnail_path }}" height="70em" />
-                                <img src="{{ $order->upper_back_thumbnail_path }}" height="70em" /> -->
-                                <!-- <br /> -->
                                 <p style="font-size: 17px">{{ $order->order_id }}</p>
                             </td>
                             <td>
@@ -176,12 +174,13 @@
 <script type="text/javascript" src="/js/administration/common.js"></script>
 <script type="text/javascript" src="/js/bootbox.min.js"></script>
 <script type="text/javascript" src="/js/administration/orders.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs-3.3.7/jqc-1.12.4/dt-1.10.13/af-2.1.3/b-1.2.4/b-colvis-1.2.4/r-2.1.0/datatables.min.js"></script>
 <script type="text/javascript">
 $(document).ready(function(){
     $('.data-table').DataTable({
         "paging": true,
         "lengthChange": false,
-        "searching": false,
+        "searching": true,
         "ordering": true,
         "info": true,
         "autoWidth": false
