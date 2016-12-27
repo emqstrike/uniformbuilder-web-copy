9963aef7fdd5b237022e22211c9a384f2b9bde56
jethqx <jethro@qstrike.com>
2016-12-27 15:27:54 +0800

    saved changes

--- resources/views/administration/patterns/patterns.blade.php
+++ resources/views/administration/patterns/patterns.blade.php
@@ -186,8 +186,6 @@
 <script type="text/javascript" src="/js/administration/common.js"></script>
 <script type="text/javascript" src="/js/administration/patterns.js"></script>
 <!-- <script type="text/javascript" src="/isotope/isotope.pkgd.min.js"></script> -->
-<script type="text/javascript" src="https://cdn.datatables.net/v/bs-3.3.7/jqc-1.12.4/dt-1.10.13/af-2.1.3/b-1.2.4/b-colvis-1.2.4/r-2.1.0/datatables.min.js"></script>
-
 <script type="text/javascript">
 $(document).ready(function(){
 
