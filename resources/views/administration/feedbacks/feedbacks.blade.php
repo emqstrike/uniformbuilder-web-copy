6fe44eb060d97b3ae60fda02a091f0825486b8f9
jethqx <jethro@qstrike.com>
2016-12-27 09:40:32 +0800

    Feedbacks view - datatables

--- resources/views/administration/feedbacks/feedbacks.blade.php
+++ resources/views/administration/feedbacks/feedbacks.blade.php
@@ -1,5 +1,11 @@
 @extends('administration.lte-main')
 
+@section('styles')
+
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs-3.3.7/jqc-1.12.4/dt-1.10.13/af-2.1.3/b-1.2.4/b-colvis-1.2.4/r-2.1.0/datatables.min.css"/>
+
+@endsection
+
 @section('content')
 
 @if (Session::has('message'))
@@ -89,12 +95,20 @@
 @endsection
 
 @section('scripts')
-<!-- <script type="text/javascript" src="/js/libs/bootstrap-table/bootstrap-table.min.js"></script> -->
+<script type="text/javascript" src="/js/libs/bootstrap-table/bootstrap-table.min.js"></script>
 <script type="text/javascript" src="/js/administration/common.js"></script>
 <script type="text/javascript" src="/jquery-ui/jquery-ui.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs-3.3.7/jqc-1.12.4/dt-1.10.13/af-2.1.3/b-1.2.4/b-colvis-1.2.4/r-2.1.0/datatables.min.js"></script>
 <script type="text/javascript">
 $(document).ready(function(){
-
+    $('.data-table').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false
+    });
 });
 </script>
 @endsection
