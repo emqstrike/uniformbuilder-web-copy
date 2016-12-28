0143a86746d1c0aa65d07af4b3e9c6cd6363b717
jethqx <jethro@qstrike.com>
2016-12-28 15:10:52 +0800

    update create and edit views of price item template managemeent - TODO - auto update of values

--- resources/views/administration/price-items/price-item-template-edit.blade.php
+++ resources/views/administration/price-items/price-item-template-edit.blade.php
@@ -22,36 +22,49 @@
                     <form class="form-horizontal" role="form" action="/administration/price_item_template/update" method="POST" enctype="multipart/form-data" id='edit-color-form'>
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <input type="hidden" name="template_id" value="{{ $template->id }}">
+                        <input type="hidden" class="template-prop" value="{{ $template->properties }}">
 
                         <div class="form-group">
-                            <label class="col-md-4 control-label">Color Name</label>
+                            <label class="col-md-4 control-label">Name</label>
                             <div class="col-md-6">
                                 <input type="name" class="form-control template-name" name="name" value="{{ $template->name }}">
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <label class="col-md-4 control-label">Sublimation Only</label>
+                            <label class="col-md-4 control-label">Description</label>
                             <div class="col-md-6">
-                                <select class="form-control" name='sublimation_only'>
-                                    <option value='0' @if ($template->sublimation_only == '0') selected @endif>No</option>
-                                    <option value='1' @if ($template->sublimation_only == '1') selected @endif>Yes</option>
-                                </select>
+                                <textarea name="description" class="form-control autosized">{{ $template->description }}</textarea>
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <label class="col-md-4 control-label">Color Code</label>
+                            <label class="col-md-4 control-label">Properties</label>
                             <div class="col-md-6">
-                                <input type="name" class="form-control template-code" name="template_code" value="{{ $template->template_code }}">
+                                <a href="#" class="btn btn-success btn-xs add-property">Add Property</a>
                             </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label class="col-md-4 control-label">Color</label>
                             <div class="col-md-6">
-                                <input id='templatepicker' />
-                                <input type='hidden' name="hex_code" id="hex-code" value="#{{ $template->hex_code }}">
+                                <table class="table table-bordered table-striped">
+                                    <tr>
+                                        <thead>
+                                            <th>Size</th>
+                                            <th>Price Item</th>
+                                            <th>Action</th>
+                                        </thead>
+                                    </tr>
+                                    <tbody class="property-body">
+<!--                                         <tr class="prop-row">
+                                            <td>
+                                                <select class="form-control sizes">
+                                                </select>
+                                            </td>
+                                            <td>
+                                                <select class="form-control price-items">
+                                                </select>
+                                            </td>
+                                        </tr> -->
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
 
@@ -76,9 +89,150 @@
 
 @endsection
 
+@section('scripts')
+<script type="text/javascript" src="/underscore/underscore.js"></script>
+<script type="text/javascript" src="/js/libs/autosize.js"></script>
+@endsection
+
 @section('custom-scripts')
 <script type="text/javascript">
 $(document).ready(function(){
+
+    var sizes = ['XS','S','M','L','XL','2XL','3XL','4XL','5XL','YS','YM','YL','YXL','Y2XL','Y3XL'];
+    var adult_sizes = ['XS','S','M','L','XL','2XL','3XL','4XL','5XL'];
+    var youth_sizes = ['YS','YM','YL','YXL','Y2XL','Y3XL'];
+    var size_properties = {};
+
+    // initSizesAndPI();
+
+    $('.autosized').autosize({append: "\n"});
+
+    getPriceItems(function(price_items){ window.price_items = price_items; });
+
+    var template_props = JSON.parse($('.template-prop').val());
+    // console.log(template_props);
+    _.each(template_props.properties.adult, function(i){
+        console.log(i);
+        var x = '<tr class="prop-row"><td><select class="form-control sizes"></select></td><td><select class="form-control price-items"></select></td></tr>';
+        y = "<td><a href='#' class='btn btn-xs btn-danger remove-prop'><span class='glyphicon glyphicon-remove'></span></a></td>";
+
+        $('.property-body').append(x);
+        $(x).append(y);
+        // var elem = '<option value="' + i + '">' + i + '</option>';
+        // $('.sizes').append(elem);
+    });
+
+    initSizesAndPI();
+
+    // _.each(template_props.properties.adult, function(i){
+    //     console.log(i);
+    //     var x = '<tr class="prop-row"><td><select class="form-control sizes"></select></td><td><select class="form-control price-items"></select></td></tr>';
+    //     y = "<td><a href='#' class='btn btn-xs btn-danger remove-prop'><span class='glyphicon glyphicon-remove'></span></a></td>";
+
+    //     $('.property-body').append(x);
+    //     $(x).append(y);
+    //     // var elem = '<option value="' + i + '">' + i + '</option>';
+    //     // $('.sizes').append(elem);
+    // });
+
+    function initSizesAndPI(){
+        _.each(sizes, function(i){
+            var elem = '<option value="' + i + '">' + i + '</option>';
+            $('.sizes').append(elem);
+        });
+
+
+        _.each(window.price_items, function(i){
+            var pi = i.price_item;
+            var elem = '<option value="' + pi + '">' + pi + '</option>';
+            $('.price-items').append(elem);
+        });
+    }
+
+    function selectChange(){
+        $( "select" ).change(function() {
+            refreshProperty();
+        });
+    }
+
+    function deleteButton(){
+        $('.remove-prop').on('click', function(e){
+            e.preventDefault();
+            $(this).parent().parent().remove();
+            refreshProperty();
+        });
+    }
+
+    function getPriceItems(callback){
+        var price_items;
+        var url = "//api-dev.qstrike.com/api/price_items";
+        $.ajax({
+            url: url,
+            async: false,
+            type: "GET",
+            dataType: "json",
+            crossDomain: true,
+            contentType: 'application/json',
+            success: function(data){
+                price_items = data['price_items'];
+                if(typeof callback === "function") callback(price_items);
+            }
+        });
+    }
+
+    function refreshProperty(){
+        console.log('refreshProperty');
+        size_properties = {};
+        var properties = {};
+        var adult = [];
+        var youth = [];
+
+        $(".prop-row").each(function(i) {
+            var x = $(this).find('.sizes').val();
+            var price_item = $(this).find('.price-items').val();
+            var prices = _.find(window.price_items, function(e){ return e.price_item === price_item; });
+
+            var data = {
+                "size" : $(this).find('.sizes').val(),
+                "price_item" : price_item,
+                "msrp" : parseFloat(prices.msrp).toFixed(2),
+                "web_price_sale" : parseFloat(prices.web_price_sale).toFixed(2)
+            }
+
+            if( _.contains(adult_sizes, data.size) ){
+                adult.push(data);
+            }
+
+            if( _.contains(youth_sizes, data.size) ){
+                youth.push(data);
+            }
+        });
+
+        if( adult.length > 0 ){
+            properties.adult = adult;
+            var adult_min_msrp = _.min(adult, function(o){return o.msrp;});
+            size_properties.adult_min_msrp = adult_min_msrp.msrp;
+            var adult_min_web_price_sale = _.min(adult, function(o){return o.web_price_sale;});
+            size_properties.adult_min_web_price_sale = adult_min_web_price_sale.web_price_sale;
+
+        }
+
+        if( youth.length > 0 ){
+            properties.youth = youth;
+            // var youth_min = _.min(youth, function(o){return o.msrp;});
+            // size_properties.youth_min = youth_min.msrp;
+            var youth_min_msrp = _.min(youth, function(o){return o.msrp;});
+            size_properties.youth_min_msrp = youth_min_msrp.msrp;
+            var youth_min_web_price_sale = _.min(youth, function(o){return o.web_price_sale;});
+            size_properties.youth_min_web_price_sale = youth_min_web_price_sale.web_price_sale;
+
+        }
+
+        size_properties.properties = properties;
+        console.log(JSON.stringify(size_properties));
+
+        // $('#size_property').val(JSON.stringify(size_properties));
+    }
 
 });
 </script>
