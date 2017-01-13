2af17abbbe716b7cd11b11be7aee4569ce7717ca
jethqx <jethro@qstrike.com>
2017-01-13 11:41:34 +0800

    save new material ui

--- public/js/administration/materials-main.js
+++ public/js/administration/materials-main.js
@@ -1,11 +1,42 @@
 $(document).ready(function() {
 
-	var $container = $('.isotope').isotope({
-	    itemSelector: '.material-div',
-	    getSortData: {
-	      category: '[data-category]'
-	    }
-	});
+	// var $container = $('.isotope').isotope({
+	//     itemSelector: '.material-div',
+	//     getSortData: {
+	//       category: '[data-category]'
+	//     }
+	// });
+
+    $('.data-table').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": true,
+        "ordering": false,
+        "info": true,
+        "autoWidth": true,
+        initComplete: function () {
+            this.api().columns().every( function () {
+                var column = this;
+                var select = $('<select><option value=""></option></select>')
+                    .appendTo( $(column.footer()).empty() )
+                    .on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+ 
+                        column
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                    } );
+
+                // column.data().unique().sort().each( function ( d, j ) {
+                //     select.append( '<option value="'+d+'">'+d+'</option>' )
+                //     // var val = $('<div/>').html(d).text();
+                //     // select.append( '<option value="' + val + '">' + val + '</option>' );
+                // } );
+            } );
+        }
+    });
 
     var filtersFlatforms="";
 
