bafb5e975328a78ec4f1b2fcadb68f7a0d01df54
Arthur Abogadil <arthur@qstrike.com>
2017-05-23 08:54:05 +0800

    Add Application Fight Shorts

--- public/uniform-builder/js/uniform-builder-applications.js
+++ public/uniform-builder/js/uniform-builder-applications.js
@@ -10333,8 +10333,10 @@ $(document).ready(function() {
             if (typeof _extra === "undefined") { 
                 ub.utilities.error('Extra Layer not detected!'); 
             } 
+            
             if (typeof _extra !== "undefined") { 
-                if (_part === "Body" || _part === "Back Body") { 
+
+                if (_part === "Body" || _part === "Back Body" || _part === "Body Left" || _part === "Body Right") { 
                     _part = 'Extra'; 
                 } 
             }
@@ -10517,7 +10519,7 @@ $(document).ready(function() {
         var _submimatedSport = ub.data.freeFormToolEnabledSports.get(ub.current_material.material.uniform_category);
         if (typeof _submimatedSport === "undefined") { return; }
 
-        // End Guard 
+        // End Guard
 
         // Select Perspective
 
@@ -10648,7 +10650,9 @@ $(document).ready(function() {
             });
 
             var _part = 'Body';
+
             if(ub.funcs.isCurrentSport('Crew Socks (Apparel)')) { _part = "Sublimated" }
+            if(ub.funcs.isCurrentSport('Wrestling') && ub.current_material.material.neck_option === "Fight Short") { _part = "Body Left" }
 
             $('div.part-container > span.part[data-id="' + _part + '"]').addClass('active');
             $('div.application-container').find('span.optionButton[data-type="mascot"]').addClass('active');
