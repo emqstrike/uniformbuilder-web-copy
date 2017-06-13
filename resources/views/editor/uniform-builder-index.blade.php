9ba56d6adde297b0a62ea52c7acf8bcfae5268a4
Arthur Abogadil <arthur@qstrike.com>
2017-06-13 16:41:15 +0800

    Add To Favorites

--- resources/views/editor/uniform-builder-index.blade.php
+++ resources/views/editor/uniform-builder-index.blade.php
@@ -49,18 +49,14 @@
                 <button id="button-return-to-customizer">Return to Customizer</button>
             </div>
 
-            <!-- 
-
             <div id="top-left-side-toolbar">
 
                 <span class="toolbar-item favorite-btn">
                     <i class="fa fa-star-o" aria-hidden="true"></i>
-                    <span class="toolbar-item-label">ADD TO<br />FAVORITE</span>
+                    <span class="toolbar-item-label">ADD TO<br />FAVORITES</span>
                 </span>    
 
             </div>
-
-            -->
 
             <div class="canvas-views" id="main_view">
             </div>
