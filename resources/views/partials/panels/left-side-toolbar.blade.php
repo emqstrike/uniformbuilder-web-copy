07713bec0f980f4001e01299ec8c139fa26e2c3f
Arthur Abogadil <arthur@qstrike.com>
2017-06-14 14:16:29 +0800

    Font Guide zIndex adjustment

--- resources/views/partials/panels/left-side-toolbar.blade.php
+++ resources/views/partials/panels/left-side-toolbar.blade.php
@@ -1,15 +1,22 @@
 <div id="left-side-toolbar">
 
-    <span class="toolbar-item undo-btn">
-        <i class="fa fa-undo" aria-hidden="true"></i><br />
-        <span class="toolbar-item-label">UNDO</span>
+    <span class="toolbar-item favorite-btn">
+        <i class="fa fa-star-o" aria-hidden="true"></i>
+        <span class="toolbar-item-label">ADD TO<br />FAVORITES</span>
     </span>    
-
-    <br />
+    
+    <br /><br /><br />
 
     <span class="toolbar-item fullscreen-btn">
         <i class="fa fa-arrows-alt" aria-hidden="true"></i><br />
         <span class="toolbar-item-label">PREVIEW</span>
     </span>        
-    
+
+    <br />
+
+    <span class="toolbar-item undo-btn">
+        <i class="fa fa-undo" aria-hidden="true"></i><br />
+        <span class="toolbar-item-label">UNDO</span>
+    </span>   
+
 </div>
