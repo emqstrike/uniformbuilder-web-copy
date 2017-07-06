f8fa3d3a5cbfc8e3a6f35d18558ddd109ab023b2
Arthur Abogadil <arthur@qstrike.com>
2017-07-06 14:33:22 +0800

    Added TeamStoreToolBox.show() and TeamStoreToolBox.close()

--- public/uniform-builder/js/TeamStoreToolBox.js
+++ public/uniform-builder/js/TeamStoreToolBox.js
@@ -3,9 +3,25 @@
  */
 
 var TeamStoreToolBox = {
+    
     init: function() {
-        console.log('Display the Team Store Toolbox');
+        
+        console.log('Prepare the Team Store Toolbox'); // Init Event handlers also, etc...
         $('#team-store-toolbox').show();
+      
+    },
+
+    show: function () {
+
+        $('#team-store-toolbox').addClass('visible')
+
+    },
+
+    close: function() {
+
+        console.log('Display the Team Store Toolbox');
+        $('#team-store-toolbox').removeClass('visible')
+        
     },
 
     create_team_store: function() {
