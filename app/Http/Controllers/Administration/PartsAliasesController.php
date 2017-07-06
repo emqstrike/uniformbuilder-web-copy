9dd1fc3dfc454a04046c4c44dacf0585bd5b6266
jethqx <kelogs1121@gmail.com>
2017-07-06 11:04:59 +0800

    added team_stores in dropzone uploader

--- app/Http/Controllers/Administration/PartsAliasesController.php
+++ app/Http/Controllers/Administration/PartsAliasesController.php
@@ -11,25 +11,32 @@ use App\Utilities\Random;
 use Aws\S3\Exception\S3Exception;
 use App\Http\Controllers\Controller;
 use App\APIClients\UniformCategoriesAPIClient;
-use App\APIClients\ApplicationSizesAPIClient as APIClient;
+use App\APIClients\PriceItemTemplatesAPIClient;
+use App\APIClients\PartsAliasesAPIClient as APIClient;
 
 class PartsAliasesController extends Controller
 {
     protected $client;
+    protected $uniformCategoriesClient;
+    protected $priceItemTemplatesAPIClient;
 
     public function __construct(
-        APIClient $apiClient
+        APIClient $apiClient,
+        UniformCategoriesAPIClient $uniformCategoriesClient,
+        PriceItemTemplatesAPIClient $priceItemTemplatesAPIClient
     )
     {
 
         $this->client = $apiClient;
+        $this->uniformCategoriesClient = $uniformCategoriesClient;
+        $this->priceItemTemplatesAPIClient = $priceItemTemplatesAPIClient;
 
     }
 
     public function index()
     {
         $parts_aliases = $this->client->getAll();
-       
+
         return view('administration.parts-aliases.parts-aliases', [
             'parts_aliases' => $parts_aliases
         ]);
@@ -37,7 +44,13 @@ class PartsAliasesController extends Controller
 
     public function addForm()
     {
-        return view('administration.parts-aliases.parts-aliases-create');
+        $sports = $this->uniformCategoriesClient->getUniformCategories();
+        $price_item_templates = $this->priceItemTemplatesAPIClient->getAll();
+
+        return view('administration.parts-aliases.parts-aliases-create', [
+            'price_item_templates' => $price_item_templates,
+            'sports' => $sports,
+        ]);
     }
 
 }
