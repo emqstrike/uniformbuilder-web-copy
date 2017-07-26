a7d1eb6ff9621b74ee508dfe47ad526037a52a43
Arthur Abogadil <arthur@qstrike.com>
2017-07-26 08:35:12 +0800

    Flip mascot on #16 / Basketball, New Basketball thumbs

--- public/uniform-builder/js/uniform-builder-applications.js
+++ public/uniform-builder/js/uniform-builder-applications.js
@@ -2780,6 +2780,7 @@ $(document).ready(function() {
 
                 if (typeof args.mascot !== "undefined" && ub.config.sport === "Football")  { _mov = ub.data.flippedMascots.getCode(args.mascot.id) && (app_id === '10' || app_id === '9'); }
                 if (typeof args.mascot !== "undefined" && ub.config.sport === "Fastpitch") { _mov = ub.data.flippedMascots.getCode(args.mascot.id) && (app_id === '9'); }
+                if (typeof args.mascot !== "undefined" && ub.config.sport === "Basketball") { _mov = ub.data.flippedMascots.getCode(args.mascot.id) && (app_id === '16'); }
 
                 if (_mov) {
 
