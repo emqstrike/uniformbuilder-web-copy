0edfb37285d77464ae92afc808b491987d0dc385
Angel-Qx <dhevs_02@yahoo.com>
2017-07-28 09:36:38 +0800

    added fabrics on properties 2

--- public/js/administration/parts-aliases.js
+++ public/js/administration/parts-aliases.js
@@ -29,8 +29,8 @@ $('.delete-part').on('click', function(){
 
    $('#confirmation-modal .confirm-yes').on('click', function(){
         var id = $(this).data('value');
-        //var url = "//api-dev.qstrike.com/api/parts_alias/delete/";
-        var url = "//localhost:8888/api/parts_alias/delete/";
+        var url = "//api-dev.qstrike.com/api/parts_alias/delete/";
+        //var url = "//localhost:8888/api/parts_alias/delete/";
        
         $.ajax({
            url: url,
