133f44fedeb9d7dc97b8b11ec795571ddb09594c
Angel-Qx <dhevs_02@yahoo.com>
2017-09-21 13:15:07 +0800

    added block pattern

--- public/js/administration/application-sizes.js
+++ public/js/administration/application-sizes.js
@@ -4,16 +4,17 @@ $(document).ready(function(){
         var data = JSON.parse($('#old_properties').val());    
         loadConfigurations(data);
     }    
-
+    
     $(".add-props").on('click', function(e) {
         e.preventDefault();
         var app_numbers_options = buildAppNumOptions();
+        var app_default_elem = '<option value="none">None</option>';
         var td_open = '<td>';
         var td_close = '</td>';
         var application_number = '<select class="form-control app-numbers" multiple="multiple">'+app_numbers_options+'</select>';
-        var size = '<input type="text" class="app-size">';
-        var scale = '<input type="text" class="app-scale">';
-        var def = '<input type="text" class="app-def">';
+        var size = '<input type="text" class="form-control app-size">';
+        var scale = '<input type="text" class="form-control app-scale">';
+        var def = '<select class="form-control app-def">'+app_default_elem+'</select>';
         var delete_row = '<a href="#" class="btn btn-danger btn-xs delete-row"><span class="glyphicon glyphicon-remove"></span></a>';
         var elem = '<tr class="layer-row">' +
                         td_open +
@@ -50,23 +51,26 @@ $(document).ready(function(){
         });
     }    
 
-    function loadConfigurations(data){
+    function loadConfigurations(data){                
         
         var app_numbers_options = buildAppNumOptions();
         var app_numbers_ref = [];
+        var default_sizes = [];
         data.forEach(function(entry, i) {                         
             var app_nums = entry.application_number;
-            app_numbers_ref.push(app_nums); 
-            var app_num_class = "app-num-"+i;            
+            app_numbers_ref.push(app_nums);
+            default_sizes.push(entry.default);
+            var app_num_class = "app-num-"+i;
+            var app_def_class = "app-def-"+i;
             var app_size = entry.size;
             var app_scale = entry.scale; 
-            var app_def = entry.default;           
+            var app_default_elem = '<option value="'+entry.default+'" selected>'+entry.default+'</option>';           
             var td_open = '<td>';
             var td_close = '</td>';
             var application_number = `<select class="form-control app-numbers `+app_num_class+`" multiple="multiple">`+app_numbers_options+`</select>`;
             var size = '<input type="text" class="form-control app-size" value="'+app_size+'">';
             var scale = '<input type="text" class="form-control app-scale" value="'+app_scale+'">';
-            var def = '<input type="text" class="form-control app-def" value="'+app_def+'">';
+            var def =  '<select class="form-control app-def '+app_def_class+'">'+app_default_elem+'</select>';
             var delete_row = '<a href="#" class="btn btn-danger btn-xs delete-row"><span class="glyphicon glyphicon-remove"></span></a>';
             var elem = '<tr class="layer-row">' +
                             td_open +
@@ -85,17 +89,30 @@ $(document).ready(function(){
                                 delete_row +
                             td_close +                          
                         '</tr>';
-            $('.properties-content').append(elem);   
-            updateJSON();
-        });
-        setTimeout(refreshSelect2s(app_numbers_ref), 1000);
+            $('.properties-content').append(elem);
+            updateJSON();            
+            setTimeout(refreshSelect2s(app_numbers_ref, entry.default), 1000);                    
+        });     
+               
     }
 
-    function refreshSelect2s(app_numbers_ref){
+    function refreshSelect2s(app_numbers_ref, default_size){
         refreshSelectBoxes();       
-        app_numbers_ref.forEach(function(entry, i) {
-            var app_num_class = ".app-num-"+i;            
-            $(app_num_class).select2('val', entry);           
+        app_numbers_ref.forEach(function(entry, i) {            
+            var app_num_class = ".app-num-"+i;    
+            var app_def_class = ".app-def-"+i;          
+            $(app_num_class).select2('val', entry);
+            var sizes = entry.toString().split(",");
+            $(app_def_class).html('');
+            var elem2 = '';            
+            sizes.forEach(function(size, j) {
+                if(default_size == size){
+                    elem2 += "<option selected>"+size+"</option>";
+                } else {
+                    elem2 += "<option>"+size+"</option>";
+                }
+            });
+            $(app_def_class).append(elem2);
         });
         updateJSON();
     }    
@@ -122,6 +139,7 @@ $(document).ready(function(){
         }
         return elem;
     }
+  
 
     function refreshSelectBoxes(){
         $(".app-numbers").each(function(i) {
@@ -132,8 +150,21 @@ $(document).ready(function(){
             });
         });       
     }
+    function setValue(thisObj){
+                
+      var selected_app_numbers = thisObj.parent().parent().find('.app-numbers').val().toString();    
+      var def_value = selected_app_numbers.split(",");
+      console.log(def_value);
+      var elem = '';
+      def_value.forEach( function(entry) {
+           elem += '<option value="'+entry+'">'+entry+'</option>';
+      });
+      thisObj.parent().parent().find('.app-def').empty().append(elem); 
+      }
 
     $("#create_application_size").on("change", ".app-numbers", function(e){
+        e.preventDefault();
+        setValue($(this));
         updateJSON();
     });
 
@@ -145,9 +176,9 @@ $(document).ready(function(){
         updateJSON();
     });
 
-    $("#create_application_size").on("keyup", ".app-def", function(e){
+    $("#create_application_size").on("change", ".app-def", function(e){        
         updateJSON();
-    });
+    });   
 
     $("#create_application_size").on("click", ".delete-row", function(e){
         updateJSON();
