1ee5be92b61fa71f064e26251db48eb3d3b4486c
Angel-Qx <dhevs_02@yahoo.com>
2017-09-27 10:59:23 +0800

    updated datatables

--- resources/views/administration/users/users.blade.php
+++ resources/views/administration/users/users.blade.php
@@ -2,7 +2,6 @@
 
 @section('styles')
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs-3.3.7/jqc-1.12.4/dt-1.10.13/af-2.1.3/b-1.2.4/b-colvis-1.2.4/r-2.1.0/datatables.min.css"/>
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css"/>
 @endsection
 
 @section('content')
