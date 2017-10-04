a6a8f62c67b3605095cf6af83e40aae36dab1522
Romack Natividad <romacknatividad@gmail.com>
2017-10-04 14:35:21 +0800

    refactored code

--- resources/views/uniform-builder.blade.php
+++ resources/views/uniform-builder.blade.php
@@ -167,9 +167,13 @@
     @include('partials.panels.order-form')
     @include('partials.panels.validate-order-form')
 
-    @if (!isset($page))
+    @if (Session::has('is_show_teamstore_toolbox'))
+
+        @if (Session::get('is_show_teamstore_toolbox'))
 
         @include('partials.panels.team-store-toolbox')
+
+        @endif
         
     @endif 
 
