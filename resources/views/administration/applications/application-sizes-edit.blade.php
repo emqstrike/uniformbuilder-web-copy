5a4801084d2060474959df2803083e6e2f150118
jethqx <kelogs1121@gmail.com>
2017-10-30 11:32:09 +0800

    fix

--- resources/views/administration/applications/application-sizes-edit.blade.php
+++ resources/views/administration/applications/application-sizes-edit.blade.php
@@ -198,15 +198,19 @@ $(function(){
         getBlockPatterns(function(block_patterns){ window.block_patterns = block_patterns; }); 
         var x = _.filter(window.block_patterns, function(e){ return e.uniform_category_id === sport; });           
                 block_patterns_value.forEach(function(pattern) {
-                    $( '#block_pattern' ).html('');
-                    $.each(x, function(i, item) {
-                        if( pattern == item.name ){
-                        $('#block_pattern' ).append( '<option value="' + item.name + '" selected>' + item.name + '</option>' );
-                        }
-                        else {
-                        $('#block_pattern' ).append( '<option value="' + item.name + '">' + item.name + '</option>' );
-                        }
-                    });
+                    try {
+                        $( '#block_pattern' ).html('');
+                        $.each(x, function(i, item) {
+                            if( pattern == item.name ){
+                            $('#block_pattern' ).append( '<option value="' + item.name + '" selected>' + item.name + '</option>' );
+                            }
+                            else {
+                            $('#block_pattern' ).append( '<option value="' + item.name + '">' + item.name + '</option>' );
+                            }
+                        });
+                    } catch(err) {
+                        console.log(err.message);
+                    }
                 });
         $('#block_pattern').trigger('change');           
     });  
@@ -225,12 +229,16 @@ $(function(){
         bps_name.forEach( function(item_name) {
             var name = item_name
             $.each(z, function(i, item) {
-               if( item.name == name ){
-                    var optx = JSON.parse(item.neck_options);
-                    $.each(optx, function(i, item) {
-                        options.push(item.name);                        
-                    });
-                } else {
+                try {
+                   if( item.name == name ){
+                        var optx = JSON.parse(item.neck_options);
+                        $.each(optx, function(i, item) {
+                            options.push(item.name);                        
+                        });
+                    } else {
+                    }
+                } catch(err) {
+                    console.log(err.message);
                 }
             });
         }); 
