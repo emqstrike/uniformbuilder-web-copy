40f5f9a277bfba1ba2ea28d1a462917b032cd6e6
jethqx <kelogs1121@gmail.com>
2017-11-21 10:40:00 +0800

    added missing dt js

--- resources/views/administration/orders/orders-sent.blade.php
+++ resources/views/administration/orders/orders-sent.blade.php
@@ -86,6 +86,7 @@
 
 @section('scripts')
 <script type="text/javascript" src="/js/libs/bootstrap-table/bootstrap-table.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs-3.3.7/jqc-1.12.4/dt-1.10.13/af-2.1.3/b-1.2.4/b-colvis-1.2.4/r-2.1.0/datatables.min.js"></script>
 <script type="text/javascript" src="/js/administration/common.js"></script>
 <script type="text/javascript" src="/js/bootbox.min.js"></script>
 <script type="text/javascript" src="/underscore/underscore.js"></script>
