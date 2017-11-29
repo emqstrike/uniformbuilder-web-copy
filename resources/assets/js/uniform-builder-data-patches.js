9a40f3390cd96620703a6dce445887d508c953cb
Arthur Abogadil <arthur@qstrike.com>
2017-11-29 11:37:10 +0800

    Added Data Patch for Goldston - 5728

--- resources/assets/js/uniform-builder-data-patches.js
+++ resources/assets/js/uniform-builder-data-patches.js
@@ -6,26 +6,38 @@ $(document).ready(function () {
 
 		// Fix save designs 4874 to 4876, pre color array fix (commit: c643b1f)
 
-		var _ids = ["4874", "4875", "4876"];
+		
+		var _idsV = ["4874", "4875", "4876"]; // Volg 
+		var _idsG = ["5728"]; // Goldstom
+	
+		if (
+			typeof ub.config.savedDesignInfo === "object" && 
+			_.contains(_idsV, ub.config.savedDesignInfo.savedDesignID)
+		) {
+			ub.current_material.settings.applications[72].color_array = _.map(["B", "W"], function (item) {
+				return ub.funcs.getColorByColorCode(item);
+			});
+
+			ub.current_material.settings.applications[73].color_array = _.map(["B"], function (item) {
+				return ub.funcs.getColorByColorCode(item);
+			});
+		}
 
 		if (
 			typeof ub.config.savedDesignInfo === "object" && 
-			_.contains(_ids, ub.config.savedDesignInfo.savedDesignID)
+			_.contains(_idsG, ub.config.savedDesignInfo.savedDesignID)
 		) {
 
-			if (_.contains(_ids, ub.config.savedDesignInfo.savedDesignID)) {
-
-				ub.current_material.settings.applications[72].color_array = _.map(["B", "W"], function (item) {
+			// Goldstom
+			if (_.contains(_idsG, ub.config.savedDesignInfo.savedDesignID)) {
+				ub.current_material.settings.applications[5].color_array = _.map(["T", "B", "VG"], function (item) {
 					return ub.funcs.getColorByColorCode(item);
 				});
-
-				ub.current_material.settings.applications[73].color_array = _.map(["B"], function (item) {
-					return ub.funcs.getColorByColorCode(item);
-				});
-
 			}
 
 		}
+		
+
 
 	}
 
