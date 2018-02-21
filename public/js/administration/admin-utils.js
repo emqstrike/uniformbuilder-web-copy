a75a9476f061b0ce465b0ef58fecc1fa74731be5
jethqx <kelogs1121@gmail.com>
2018-02-21 11:59:31 +0800

    added method for stringify->parse json

--- public/js/administration/admin-utils.js
+++ public/js/administration/admin-utils.js
@@ -16,7 +16,10 @@ $(document).ready(function() {
 	window.au.util = {
 		escapeSingleDoubleQuotes: function (jsonString) {
 			return jsonString.replace(/(['"])/g, "\\$1");
-		}
+		},
+		stringifyThenJSONParse: function (jsonObject) {
+			return JSON.parse(JSON.stringify(jsonObject));
+		},
 	};
 
 	au.applications.events = function(){
