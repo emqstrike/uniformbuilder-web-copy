62de8b0acd1ad55ef65f7bd104e9260a9d6d1860
Arthur Abogadil <arthur@qstrike.com>
2018-07-19 14:13:58 +0800

    (unused code, Mascots) CCO-272: Remove duplicate code

--- resources/assets/js/uniform-builder.js
+++ resources/assets/js/uniform-builder.js
@@ -863,31 +863,6 @@ $(document).ready(function () {
 
         };
 
-
-        ub.data.mascotsCategories = {};
-
-        ub.funcs.transformMascots = function () {
-    
-            ub.data.mascots = _.filter (ub.data.mascots, {active: '1'});
-
-            // Hide Richardson Mascots #Richardson #BrandSpecific
-            if (!_.contains(ub.fontGuideIDs, window.ub.valid)) {
-                ub.data.mascots = _.filter(ub.data.mascots, {brand: 'prolook'})
-            }
-
-            _.each(ub.data.mascots, function (mascot, index) {
-
-                mascot.layers_properties = JSON.parse(mascot.layers_properties);
-
-                ub.data.mascotsCategories[mascot.category] = {
-                    name: mascot.category,
-                    id: mascot.mascot_category_id,
-                };
-
-            });
-
-        }
-
         /// Load Assets 
 
         ub.callback_update = function (obj, object_name) {
