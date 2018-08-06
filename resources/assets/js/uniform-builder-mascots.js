a1f81c4b81764fa5416c2c4fbce273dee21160ae
Arthur Abogadil <arthur@qstrike.com>
2018-08-06 18:05:33 +0800

    Mascot Category ID fix

--- resources/assets/js/uniform-builder-mascots.js
+++ resources/assets/js/uniform-builder-mascots.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
 
     ub.funcs.transformMascots = function () {
 
-        var _one = '1';
+        var _one = 1;
 
-        if (ub.config.toString) { _one = 1; }
+        if (ub.config.toString) { _one = '1'; }
 
-        ub.data.mascots = _.filter (ub.data.mascots, {active: '1'});
+        ub.data.mascots = _.filter (ub.data.mascots, {active: _one});
 
         // Hide Richardson Mascots #Richardson #BrandSpecific
         if (!_.contains(ub.fontGuideIDs, window.ub.valid)) {
@@ -17,11 +17,17 @@ $(document).ready(function() {
 
         _.each(ub.data.mascots, function (mascot, index) {
 
+            var _mascotID = mascot.mascot_category_id;
+
+            if (_mascotID !== null) { 
+                if (ub.config.toString) { _mascotID = mascot.mascot_category_id.toString(); } 
+            }
+
             mascot.layers_properties = JSON.parse(mascot.layers_properties);
 
             ub.data.mascotsCategories[mascot.category] = {
                 name: mascot.category,
-                id: mascot.mascot_category_id,
+                id: _mascotID,
             };
 
         });
