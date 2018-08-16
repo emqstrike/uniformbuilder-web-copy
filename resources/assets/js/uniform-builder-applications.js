7edc0eb8f0902662b377085f60559ea9a2b0e1bf
Elmer Alvarado <elmer@qstrike.com>
2018-08-16 15:33:50 +0800

    (improvement) CCO-253: Enable Opacity on Materials
    Opacity on materials (if any) should be displayed accordingly.
    Update the PDF, put the pattern opacity percentage in PARTS section

--- resources/assets/js/uniform-builder-applications.js
+++ resources/assets/js/uniform-builder-applications.js
@@ -3762,7 +3762,7 @@ $(document).ready(function() {
                 var _obj    = _.find(ub.data.modifierLabels, {fullname: _result});
                 var _index  = ub.funcs.getIndexByName(_result);
 
-                ub.funcs.activatePartByIndex(_index);   
+                ub.funcs.activatePartByIndex(_index);
 
             }
             else {
