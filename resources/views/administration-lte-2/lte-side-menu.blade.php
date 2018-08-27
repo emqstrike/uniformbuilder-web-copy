fbeb80d9b93128a85151229718c655dfac8c474e
unknown <angel@qstrike.com>
2018-08-27 14:50:49 +0800

    added mascot sizes on side menu

--- resources/views/administration-lte-2/lte-side-menu.blade.php
+++ resources/views/administration-lte-2/lte-side-menu.blade.php
@@ -165,6 +165,7 @@
           </a>
           <ul class="treeview-menu">
             <li><a href="/administration/{{ env('ENDPOINT_VERSION') }}/application_sizes"><i class="fa fa-arrows"></i>Application Sizes<span class="pull-right-container"><small class="label pull-right bg-green"><i class="fa fa-exclamation"></i></small></span></a></li>
+            <li><a href="/administration/{{ env('ENDPOINT_VERSION') }}/mascot_sizes"><i class="fa fa-arrows"></i>Mascot Sizes<span class="pull-right-container"><small class="label pull-right bg-green"><i class="fa fa-exclamation"></i></small></span></a></li>
           </ul>
         </li>
 
