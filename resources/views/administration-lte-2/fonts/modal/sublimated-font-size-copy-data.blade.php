64b646869def7e9d916cdfb112467780d69009d3
Alvin Manansala <aomanansala@gmail.com>
2018-09-14 17:10:20 +0800

    include missing files

---
+++ resources/views/administration-lte-2/fonts/modal/sublimated-font-size-copy-data.blade.php
@@ -0,0 +1,18 @@
+<div class="modal fade" id="myModalB" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">×</button>
+                <h4> Sublimated font size copy data</h4>
+            </div>
+
+            <div class="modal-body">
+                <form role="form">
+                    <div>
+                        <textarea class="fst-sublimated-data-field form-control animated" style="height: 300px; resize: none;"></textarea>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
