0c3c16d1d34629616739aed5157e5c3f455a2627
unknown <angel@qstrike.com>
2018-10-09 13:46:49 +0800

    added feedbacks on new Admin UI

--- resources/views/administration-lte-2/lte-side-menu.blade.php
+++ resources/views/administration-lte-2/lte-side-menu.blade.php
@@ -106,7 +106,7 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="../tables/simple.html"><i class="fa fa-comments"></i> Feedbacks</a></li>
+            <li><a href="/administration/{{ env('ENDPOINT_VERSION') }}/feedbacks"><i class="fa fa-comments"></i> Feedbacks<span class="pull-right-container"><small class="label pull-right bg-green"><i class="fa fa-exclamation"></i></small></span></a></li>
             <li><a href="/administration/{{ env('ENDPOINT_VERSION') }}/inksoft_designs"><i class="fa fa-object-ungroup"></i> Inksoft Designs</a></li>
             <li><a href="../tables/data.html"><i class="fa fa-linux"></i> Logo Requests</a></li>
             <li><a href="{{ route('saved_designs') }}"><i class="fa fa-image"></i> Saved Designs</a></li>
