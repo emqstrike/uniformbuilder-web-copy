a593cf1bdcc9e3b9c8cf81841b2f36076cc8b7c4
Aron Joshua Bagtas <aaron@qstrike.com>
2018-10-18 13:42:45 +0800

    Colors and pattern css

--- resources/views/partials/panels/mod_primary.blade.php
+++ resources/views/partials/panels/mod_primary.blade.php
@@ -35,150 +35,10 @@
     
     </div>
 
-    <div class="options_panel_section modifier_main_container" id="primary_options_containers">
-        <div class="" style="text-transform: uppercase; color: #ffffff;">
-            <h4 class="header" style="font-weight: bolder;">Base Color</h4>
-            <p style="font-weight: bold;">Choose color / Pattern</p>
-        </div>
-        <div class="ubColorPicker" data-index="undefined" data-type="single" data-option="Team-color-picker" data-group="colors">
-            <p>COLOR</p>
-            <div class="col-md-12">
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-            </div>
-            <p>PATTERN</p>
-            <div class="col-md-12" style="margin-bottom: 10px !important">
-                <div class="color_element sublimated">
-                    <button class="grow btn change-color whitebtn" style="background-color: violet; width: 38px; height: 25px; border-radius: 0px; border-width: thin; padding: 0px;">
-                    </button>
-                </div>
-            </div>
-            <button style="width: 150px !important; padding: 10px !important; font-size: 12px !important; color: black !important;" data-toggle="modal" data-target="#pattern-change-color">
-                Edit pattern color
-            </button>
-            <hr>
-        </div>
-
-        {{-- <div id="primary_options_colors" class="mod_primary_panels" data-type="color" style='float: left; display: none;'>
-
+    <div class="options_panel_section modifier_main_container" id="primary_options_container">
+        
+        <div id="primary_options_colors" class="mod_primary_panels" data-type="color" style='float: left; display: none;'>
+            
         </div>
 
         <div id="primary_options_patterns" class="mod_primary_panels" data-type="pattern">
@@ -203,44 +63,36 @@
 
             <div class="primary_header">Number </div>
 
-        </div> --}}
+        </div>
 
     </div>
 
     <!-- 
-
     <div id="bottom_modifier_tabs">
-
             <a class="footer-buttons change-view" data-view="front">
                 F
                 <br /><span>Front View</span>
             </a>
-
             <a class="footer-buttons change-view" data-view="back">
                 B
                 <br /><span>Back View</span>
             </a>
-
             <a class="footer-buttons change-view" data-view="left">
                 L
                 <br /><span>Left View</span>
             </a>
-
             <a class="footer-buttons change-view" data-view="right">
                 R
                 <br /><span>Right View</span>
             </a>
-
             <a class="footer-buttons change-view" data-view="zoom">
                 <i class="fa fa-search"></i>
                 <br /><span>Zoom</span>
             </a>
-
             <a class="footer-buttons change-view" data-view="home">
                 <i class="fa fa-home"></i>
                 <br /><span>Home</span>
             </a>
-
         
         <div class='tabs'>
         
@@ -263,108 +115,14 @@
             <div class="mTab" data-type="number">
                 <img src="/images/uiV1/modifier_tabs/inactive/number.png">
             </div>
-
         </div>
-
         <div id="next_container">
             
             <button id="prev_mo"><i class="fa fa-arrow-left" aria-hidden="true"></i></button><button id="next_mo">Shoulder Cowl Insert <br /> Next Part
             </button>
-
         </div>
-
-
     </div>
-
     -->
 
 
 </div> <!-- End Sizes Panel -->
-
-<!-- Modal -->
-<div class="modal fade" id="pattern-change-color" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog modal-md" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Pattern Color</h4>
-      </div>
-      <div class="modal-body">
-        <div class="row">
-            <div class="col-md-3">
-                <img src="http://i.pravatar.cc/300" alt="" width="100%" height="150">
-            </div>
-            <div class="col-md-9">
-                <!-- Nav tabs -->
-                <div class="row">
-                    <div class="col-sm-3 btn btn-default">
-                        1
-                    </div>
-                    <div class="col-sm-3 btn btn-default">
-                        2
-                    </div>
-                    <div class="col-sm-3 btn btn-default">
-                        3
-                    </div>
-                    <div class="col-sm-3 btn btn-default">
-                        4
-                    </div>
-                </div>
-                <hr>
-                <div class="row" style="text-align: center;">
-                    <div class="col-sm-2" style="margin: 5px; background-color: green !important; height: 20px">
-
-                    </div>
-                    <div class="col-sm-2" style="margin: 5px; background-color: pink !important; height: 20px">
-
-                    </div>
-                    <div class="col-sm-2" style="margin: 5px; background-color: black !important; height: 20px">
-
-                    </div>
-                    <div class="col-sm-2" style="margin: 5px; background-color: gray !important; height: 20px">
-
-                    </div>
-                    <div class="col-sm-2" style="margin: 5px; background-color: lightgreen !important; height: 20px">
-
-                    </div>
-                    <div class="col-sm-2" style="margin: 5px; background-color: green !important; height: 20px">
-
-                    </div>
-                    <div class="col-sm-2" style="margin: 5px; background-color: pink !important; height: 20px">
-
-                    </div>
-                    <div class="col-sm-2" style="margin: 5px; background-color: black !important; height: 20px">
-
-                    </div>
-                    <div class="col-sm-2" style="margin: 5px; background-color: gray !important; height: 20px">
-
-                    </div>
-                    <div class="col-sm-2" style="margin: 5px; background-color: lightgreen !important; height: 20px">
-
-                    </div>
-                    <div class="col-sm-2" style="margin: 5px; background-color: green !important; height: 20px">
-
-                    </div>
-                    <div class="col-sm-2" style="margin: 5px; background-color: pink !important; height: 20px">
-
-                    </div>
-                    <div class="col-sm-2" style="margin: 5px; background-color: black !important; height: 20px">
-
-                    </div>
-                    <div class="col-sm-2" style="margin: 5px; background-color: gray !important; height: 20px">
-
-                    </div>
-                    <div class="col-sm-2" style="margin: 5px; background-color: lightgreen !important; height: 20px">
-
-                    </div>
-                </div>
-            </div>
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
