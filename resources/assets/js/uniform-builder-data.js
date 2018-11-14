996ede67a2dab373c20f213900b79c573bca4c7f
Elmer Alvarado <elmer@qstrike.com>
2018-11-14 16:09:13 +0800

    (FEEDBACK) FEED-24: Austin Morrison feedback disable matching ids on Baseball

--- resources/assets/js/uniform-builder-data.js
+++ resources/assets/js/uniform-builder-data.js
@@ -10080,22 +10080,33 @@ ub.funcs.fontOffSets = [
             { id: 44, matchingID: 43  },
             { id: 41, matchingID: 42  },
             { id: 42, matchingID: 41  },
-        ], 
+        ],
+        sportsExemption: [
+            'Baseball'
+        ],
         getMatchingID: function (id) {
 
             var _result = undefined;
+            var sport   = ub.config.sport;
 
-            _result = _.find(this.items, {id: parseInt(id)});
+            // Igore matching id on Baseball
+            // see FEED-24 for details
+            if (!_.contains(this.sportsExemption, sport)) {
 
-            if (typeof _result === "undefined") {
+                _result = _.find(this.items, {id: parseInt(id)});
 
-                return undefined;
+                if (typeof _result === "undefined") {
 
+                    return undefined;
+
+                }
+
+                return _result.matchingID;
             }
 
-            return _result.matchingID;
+            return _result;
 
-        } 
+        }
 
     }
 
