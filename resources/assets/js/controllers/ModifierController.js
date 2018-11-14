06fb2e487b8a2046124d4ab926530ff4b48f0508
rodz3rd2 <rodrigogalura3rd@gmail.com>
2018-11-14 11:26:19 +0800

    apply the standard format

--- resources/assets/js/controllers/ModifierController.js
+++ resources/assets/js/controllers/ModifierController.js
@@ -173,73 +173,25 @@ ModifierController.prototype = {
         if (ub.funcs.popupsVisible()) { return; }
         if (!ub.funcs.okToStart())    { return; }
 
-        ub.funcs.activatePanelGuard();
-        ub.funcs.deactivatePanels();
-
-        $('#primary_options_container').html("");
+        var properties_panel = new PropertiesPanel("#primary_options_container", "Richardsons");
 
         if (PipingPanel.isValidToProcessPipings()) {
-            // get the name of pipings
-            var piping_types = ub.funcs.getPipingSets();
-            var piping_sidebar_tmpl = $('#m-piping-sidebar-new').html();
+            ub.funcs.activatePanelGuard();
+            ub.funcs.deactivatePanels();
 
-            var piping_set_items = _.map(piping_types, function(piping_type) {
-                var active_piping_set = ub.current_material.settings.pipings[piping_type];
-                var piping_set = piping_type;
+            ub.modifierController.pipings = new PipingPanel('m-piping-sidebar-new');
+            ub.modifierController.pipings.setPipingSetItems();
 
-                if (active_piping_set !== "undefined") {
-                    piping_set = ub.funcs.getPipingSet(piping_type);
-                    active_piping_set = _.first(piping_set);
-                }
+            var piping_panel = ub.modifierController.pipings.getPanel();
+            properties_panel.setBodyPanel(piping_panel);
 
-                var sizes = ub.funcs.sortPipingSizes({items: piping_set});
-                var colors = ub.funcs.getPipingColorArray(active_piping_set);
-
-                var modifier = piping_type.toLowerCase().replace(/ /g, "-")
-
-                return {
-                    sizes: sizes.items,
-                    colors: colors,
-                    type: piping_type,
-                    type_wo_left_prefix: piping_type.indexOf('Left') === 0 ? piping_type.replace("Left", "") : piping_type,
-                    modifier: modifier
-                };
-            });
-
-            $('.modifier_main_container').append(Mustache.render(piping_sidebar_tmpl, {
-                piping_set_items: piping_set_items,
-            }));
-
-            // PipingPanel.initEvents();
             PipingPanel.events.init();
-
-            // set initial states
-            _.map(piping_types, function(piping_type) {
-                var active_piping_set = PipingPanel.getActivePipingSet(piping_type);
-                var status = PipingPanel.getPipingPanelStatus(piping_type);
-                var pipping_settings_object = ub.funcs.getPipingSettingsObject(active_piping_set.set);
-
-                var piping_item_el = $('#pipingsUI .piping-item[data-piping-type="'+piping_type+'"]');
-
-                if (pipping_settings_object.enabled === 1 && pipping_settings_object.size !== "") {
-                    $('.piping-sizes-buttons[data-size="' + pipping_settings_object.size + '"]', piping_item_el).click();
-                }
-
-                var temporary_status = status === PipingPanel.STATUS_ON ? PipingPanel.STATUS_OFF : PipingPanel.STATUS_ON;
-
-                $('.toggle', piping_item_el).data('status', temporary_status);
-                $('.toggleOption.'+temporary_status, $('.toggle', piping_item_el)).click();
-            });
-
-            $('#pipingsUI').fadeIn();
+            PipingPanel.setInitialState();
         } else {
-            var no_piping_msg_tmpl = $('#m-no-piping-message').html();
+            ub.modifierController.pipings = new PipingPanel('m-no-piping-message');
 
-            $('#no-piping-message').remove();
-            $('.modifier_main_container').append(no_piping_msg_tmpl);
-            $('#no-piping-message').fadeIn();
-
-            $("#primary_options_colors").remove();
+            var piping_panel = ub.modifierController.pipings.getNoPipingPanel();
+            properties_panel.setBodyPanel(piping_panel);
         }
     },
 
