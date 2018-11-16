a41a7a07c0ef6f77425cded36b91287afd3bc611
Romack Natividad <romacknatividad@gmail.com>
2018-11-16 17:42:37 +0800

    Switch perspective on object property change

--- resources/assets/js/panels/NumberPanel.js
+++ resources/assets/js/panels/NumberPanel.js
@@ -63,18 +63,43 @@ NumberPanel.prototype = {
         // Handler for application's rotation level
     },
 
-    setNumber: function() {
-        var panel = ub.modifierController.numbers;
-        // change number
-        var application_type = $(this).data('application-type');
-        var font_id = $(this).data('font-id');
-        var number = $(this).val();
-        var settings = panel.getApplicationByType(application_type);
+    getFont: function(font_id) {
+        var font = _.find(ub.data.fonts, {
+            id: font_id.toString()
+        });
+        return font;
+    },
 
-        if (typeof settings !== 'undefined') {
-            settings.text = number;
-            ub.funcs.changeFontFromPopup(settings.font_obj.id, settings);
-            ub.utilities.actionLog('Updated Number on ' + application_type + ' to ' + number);
+    configurePerspective: function(layer) {
+        console.log('layer = ' + layer);
+        var perspective = new PerspectiveController;
+        if (layer.toLowerCase().indexOf('back') > -1) {
+            perspective.back();
+        } else if (layer.toLowerCase().indexOf('front') > -1) {
+            perspective.front();
+        } else if (layer.toLowerCase().indexOf('right') > -1) {
+            perspective.right();
+        } else if (layer.toLowerCase().indexOf('left') > -1) {
+            perspective.left();
+        } else {
+            ub.utilities.error('Unable to find perspective');
+        }
+        return perspective;
+    },
+
+    // Apply the changes to the stage
+    applyNumberChanges: function(font_id, application_settings, layer) {
+        var font = this.getFont(font_id);
+        var perspective = this.configurePerspective(layer);
+
+        console.log(application_settings);
+        var object_id = null;
+
+        if (typeof application_settings !== 'undefined') {
+            object_id = application_settings.id;
+            ub.funcs.removeApplicationByID(object_id);
+            application_settings.font_obj = font;
+            ub.create_application(application_settings, undefined);
         } else {
             ub.utilities.error('Missing object settings. Unable to apply changes.');
         }
@@ -92,15 +117,35 @@ NumberPanel.prototype = {
         return application_obj;
     },
 
+    setNumber: function() {
+        var panel = ub.modifierController.numbers;
+        // change number
+        var application_type = $(this).data('application-type');
+        var font_id = $(this).data('font-id');
+        var number = $(this).val();
+        var layer = $(this).data('application-layer');
+        var application_settings = panel.getApplicationByType(application_type);
+
+        if (typeof application_settings !== 'undefined') {
+            application_settings.text = number;
+            panel.applyNumberChanges(font_id, application_settings, layer);
+            ub.utilities.actionLog('Updated Number on ' + application_type + ' to ' + number);
+        } else {
+            ub.utilities.error('Missing object settings. Unable to apply changes.');
+        }
+    },
+
     setFontStyle: function() {
         var panel = ub.modifierController.numbers;
         // change font style
+        var application_type = $(this).data('application-type');
+        var layer = $(this).data('application-layer');
         var font_id = $(this).val();
         console.log('Font ID = ' + font_id);
-        var settings = panel.getApplicationByType(application_type);
+        var application_settings = panel.getApplicationByType(application_type);
 
-        if (typeof settings !== 'undefined') {
-            ub.funcs.changeFontFromPopup(font_id, settings);
+        if (typeof application_settings !== 'undefined') {
+            panel.applyNumberChanges(font_id, application_settings, layer);
             ub.utilities.actionLog('Updated Font on ' + application_type + ' to ' + font_id);
         } else {
             ub.utilities.error('Missing object settings. Unable to apply changes.');
