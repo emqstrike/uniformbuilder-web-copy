98a82b44de28106bf0770acde4c49067b4ab8b20
ayellutero <ayellutero@gmail.com>
2018-12-04 11:57:16 +0800

    modify clickable condition

--- resources/assets/js/controllers/ModifierController.js
+++ resources/assets/js/controllers/ModifierController.js
@@ -248,10 +248,11 @@ ModifierController.prototype = {
 };
 
 ModifierController.scrollToOptions = function (application_type, application_id) {
+    console.log('TYPE: ' + application_type)
     // Check if clicked application is TEAM NAME or PLAYER NAME,
     if (application_type === "team_name" || application_type === "player_name") {
         $('#property-modifiers-menu .menu-item-letters').trigger('click')
-    } else if (application_type === "front_number" || application_type === "back_number" || application_type === "sleeve_number") {
+    } else if (application_type === "front_number" || application_type === "back_number" || application_type === "sleeve_number" || application_type === "number") {
         // Numbers
         $('#property-modifiers-menu .menu-item-numbers').trigger('click')
     } else if (application_type === "mascot" || application_type === "embellishments") {
