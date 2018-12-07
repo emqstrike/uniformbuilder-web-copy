3339846262a7446c9a90e7ba13f186e440774aae
rodz3rd2 <rodrigogalura3rd@gmail.com>
2018-12-07 08:24:52 +0800

    add no fabric available template

--- resources/views/partials/controls/properties/fabrics.blade.php
+++ resources/views/partials/controls/properties/fabrics.blade.php
@@ -1,27 +1,35 @@
 <script type="x-tmpl-mustache" id="fabric-tmpl">
-    <div class="fabric-container" style="">
-        <h4>FABRIC</h4>
-        <h5>CHOOSE BASE FABRIC</h5>
+    @{{ #fabrics }}
+        <div class="fabric-container">
+            <h4>FABRIC</h4>
+            <h5>CHOOSE BASE FABRIC</h5>
 
-        <div class="fabric-layers">
-            @{{ #fabric_layers }}
-                <div class="fabric-layer" data-layer-level="@{{ layer_level }}">
-                    <div class="image-wrapper">
-                        <img src="@{{ thumbnail }}" class="img-responsive @{{ class_active }}" alt="" />
-                    </div>
-                    <div class="fabric-description">
-                        <h4 class="fabric-layer-title">@{{ layer_title }} (@{{ layer_level_category }})</h4>
+            <div class="fabric-layers">
+                @{{ #fabric_layers }}
+                    <div class="fabric-layer" data-layer-level="@{{ layer_level }}">
+                        <div class="image-wrapper">
+                            <img src="@{{ thumbnail }}" class="img-responsive @{{ class_active }}" alt="" />
+                        </div>
+                        <div class="fabric-description">
+                            <h4 class="fabric-layer-title">@{{ layer_title }} (@{{ layer_level_category }})</h4>
 
-                        <ul class="description-list">
-                            @{{ #description_list }}
-                                <li>
-                                    <p>@{{ description }}</p>
-                                </li>
-                            @{{ /description_list }}
-                        </ul>
+                            <ul class="description-list">
+                                @{{ #description_list }}
+                                    <li>
+                                        <p>@{{ description }}</p>
+                                    </li>
+                                @{{ /description_list }}
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            @{{ /fabric_layers }}
+                @{{ /fabric_layers }}
+            </div>
         </div>
-    </div>
+    @{{ /fabrics }}
+
+    @{{ ^fabrics }}
+        <div class="fabric-container">
+            <h4>No Fabric Available</h4>
+        </div>
+    @{{ /fabrics }}
 </script>
