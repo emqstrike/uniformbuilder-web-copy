13536f0405f81dc684285db92a4116f91b502403
ayellutero <ayellutero@gmail.com>
2018-12-17 16:17:13 +0800

    Revert change for patterned socks

--- resources/assets/js/uniform-builder-patterns.js
+++ resources/assets/js/uniform-builder-patterns.js
@@ -884,7 +884,6 @@ $(document).ready(function () {
         
         _htmlBuilder     += "</div>";
 
-        $('div#randomFeedsUI').hide();
         $('.modifier_main_container').append(_htmlBuilder);
 
         ub.funcs.setupPartPatternSlider(inputPattern, materialOption);
