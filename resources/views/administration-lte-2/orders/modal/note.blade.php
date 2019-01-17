a86266f5e5f0faa7110bc0c17359dd47c3448c30
Alvin Manansala <aomanansala@gmail.com>
2019-01-17 18:21:09 +0800

    filter orders by delete

---
+++ resources/views/administration-lte-2/orders/modal/note.blade.php
@@ -0,0 +1,14 @@
+<div id="note-modal" class="modal fade" data-backdrop="static" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">×</button>
+                <h4>Note</h4>
+            </div>
+
+            <div class="modal-body">
+                <textarea class="form-control" id="notes" style="resize: none; height: 100px;" disabled></textarea>
+            </div>
+        </div>
+    </div>
+</div>
