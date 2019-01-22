43b7c90443b1fa494535cf1580a7ba48e03b368f
Aron Joshua Bagtas <aaron@qstrike.com>
2019-01-22 15:58:40 +0800

    Add ProDry in reject list for Free Form Layer

--- resources/assets/js/uniform-builder-applications.js
+++ resources/assets/js/uniform-builder-applications.js
@@ -10330,8 +10330,8 @@ $(document).ready(function() {
                 
             });
 
-            _list = _.reject(_list, function (item)     { return item.name.indexOf('Trim') > -1 || 
-                                                                 item.name.indexOf('Piping') > -1 || 
+            _list = _.reject(_list, function (item)     { return item.name.indexOf('Trim') > -1 ||
+                                                                 item.name.indexOf('Piping') > -1 ||
                                                                  item.name.indexOf('Stripe') > -1 ||
                                                                  item.name.indexOf('Front Insert') > -1 ||
                                                                  item.name.indexOf('Zipper') > -1 ||
@@ -10339,7 +10339,8 @@ $(document).ready(function() {
                                                                  item.name.indexOf('Hood Cuff') > -1 ||
                                                                  item.name.indexOf('Pocket Cuff') > -1 ||
                                                                  item.name.indexOf('Arm Cuff') > -1 ||
-                                                                 item.name.indexOf('Prolook') > -1 || 
+                                                                 item.name.indexOf('Prolook') > -1 ||
+                                                                 item.name.indexOf("Pro-Dry") > -1 ||
                                                                  item.name.indexOf('Belt Loop') > -1; });
 
             if (ub.funcs.isSocks()) { 
