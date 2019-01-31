6a9ac02b9f3c1d6369e31b600ee1c2e357818094
Aron Joshua Bagtas <aaron@qstrike.com>
2019-01-31 13:18:55 +0800

    Richardson change perspective

--- resources/assets/js/uniform-builder-ui-functions.js
+++ resources/assets/js/uniform-builder-ui-functions.js
@@ -11,14 +11,66 @@ $(document).ready(function() {
         $("h1#header_text").css('display', 'none');
     };
 
-    ub.funcs.disbleSidebarLeft = function() {
-        $("div#left-side-toolbar").css("display", 'none');
+    ub.funcs.setupSidePanelToolbar = function() {
+        ub.front_view.visible = true;
+        ub.back_view.visible = true;
+        ub.left_view.visible = true;
+        ub.right_view.visible = true;
+
+        // Setup perspective
+        var perspectives = [
+            {
+                "perspective": "front",
+                "image": ub.getThumbnailImage("front_view")
+            },
+            {
+                "perspective": "back",
+                "image": ub.getThumbnailImage("back_view")
+            },
+            {
+                "perspective": "left",
+                "image": ub.getThumbnailImage("left_view")
+            },
+            {
+                "perspective": "right",
+                "image": ub.getThumbnailImage("right_view")
+            }
+        ];
+
+        var template = document.getElementById("m-left-panel-toolbar");
+        var rendered_template = Mustache.render(template.innerHTML, {perspectives: perspectives});
+        $("div#left-side-toolbar").html(rendered_template);
+
+        // Add CSS in left side toolbar
+        $("div#left-side-toolbar").css({
+            "top": '150px',
+            "left": '50px'
+        });
+
     };
 
+    ub.funcs.handlePerspectiveEvent = function() {
+        $("div#left-side-toolbar .perspective").on('click', '.change-perspective-button', function(event) {
+            event.preventDefault();
+            /* Act on the event */
+            var view = $(this).data('perspective');
+            var perspective = new PerspectiveController();
+            perspective.setPerspective(view)
+        });
+    }
+
+
+
     ub.funcs.changeStage = function() {
+        $("div#left-side-toolbar").html("")
+        $("div.customizer-uniform-information").html("");
         ub.funcs.changeStageBackgroundColor("0xffffff");
         ub.funcs.removeApplicationHeader();
-        ub.funcs.disbleSidebarLeft();
+
+        _.delay(function() {
+            ub.funcs.setupSidePanelToolbar();
+            ub.funcs.handlePerspectiveEvent();
+        }, 1500)
     }
 
 })
