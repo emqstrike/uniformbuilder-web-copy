88d7ce3b1954d1301f1aa7fd6267413194d643d4
Aron Joshua Bagtas <aaron@qstrike.com>
2019-02-01 13:44:24 +0800

    Richardson footer navigator

--- resources/views/partials/controls/properties/richardson-skin.blade.php
+++ resources/views/partials/controls/properties/richardson-skin.blade.php
@@ -1,9 +1,9 @@
 <script type="text/mustache" id="m-left-panel-toolbar">
     <div class="perspective">
         @{{ #perspectives }}
-            <a href="javascript:void(0)" class="change-perspective-button" data-perspective="@{{ perspective }}">
-                <div class="cp-padding-xs cp-width-xs cp-padding-remove-horizontal">
-                    <div class="cp-border cp-padding-xs">
+            <a href="javascript:void(0)" class="change-perspective-button cp-padding-xs" data-perspective="@{{ perspective }}">
+                <div class="cp-padding-xs cp-width-xs cp-padding-remove-horizontal cp-border">
+                    <div class= cp-padding-xs">
                         <img src="@{{ image }}" alt="" width="100%" height="50">
                     </div>
                 </div>
@@ -31,6 +31,51 @@
             </div>
             <div>
                 <h6 class="cp-text-uppercase cp-text-bold cp-fc-red cp-padding-xs cp-margin-remove cp-padding-remove-horizontal">@{{ application_type }}</h6>
+            </div>
+        </div>
+    </div>
+</script>
+
+<script type="text/mustache" id="m-richardson-footer">
+    <div class="richardson-footer">
+        <div class="cp-padding-tiny cp-bgc-light">
+            <div class="cp-border cp-border-black">
+                <div class="row">
+                    <a href="javascript:void(0)" class="richardson-onPrevious cp-fc-black">
+                        <div class="col-md-1 cp-padding-remove">
+                            <div class="richardson-arrow-left cp-padding-tiny">
+                                <span class="fa fa-arrow-left cp-fc-red cp-text-center"></span>
+                            </div>
+                        </div>
+                        <div class="col-md-5 cp-padding-remove">
+                            <div class="cp-border cp-border-black">
+                                <h4 class="cp-text-uppercase cp-text-center">Previous</h4>
+                            </div>
+                        </div>
+                    </a>
+                    <a href="javascript:void(0)" class="richardson-onNext cp-fc-black">
+                        <div class="col-md-5 cp-padding-remove">
+                            <div class="cp-border cp-border-black">
+                                <h4 class="cp-text-uppercase cp-text-center">Next</h4>
+                            </div>
+                        </div>
+                        <div class="col-md-1 cp-padding-remove">
+                            <div class="richardson-arrow cp-padding-tiny">
+                                <span class="fa fa-arrow-right cp-fc-red cp-text-center"></span>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="cp-width-1-1">
+                <div class="cp-padding-xs cp-bgc-darkerGray">
+                    <h4 class="cp-text-uppercase cp-text-center cp-margin-remove cp-fc-white">Finish / View Summary</h4>
+                </div>
+            </div>
+            <div class="cp-width-1-1">
+                <div class="cp-padding-xs">
+                    
+                </div>
             </div>
         </div>
     </div>
