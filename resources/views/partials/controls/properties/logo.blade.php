c3d605c20c2ff2306ae83937364fbc7c0eead6be
Elmer Alvarado <elmer@qstrike.com>
2019-02-04 09:10:56 +0800

    fix richardson logo perspective button

--- resources/views/partials/controls/properties/logo.blade.php
+++ resources/views/partials/controls/properties/logo.blade.php
@@ -4,9 +4,9 @@
             <h3 class="cp-fc-white cp-margin-remove cp-text-uppercase cp-text-bold">@{{ brand }} Logo</h3>
             <p class="cp-text-uppercase cp-fc-white cp-text-medium cp-padding-small cp-padding-remove-horizontal cp-margin-remove">Choose Location</p>
         </div>
-        <div class="row logo-perspective-btn-container">
+        <div class="logo-perspective-btn-container">
             @{{ #logo_position }}
-                <div class="col-md-6">
+                <div class="logo-perspective">
                     <button class="cp-width-1-1 cp-tab-button cp-border-none cp-text-medium logo-perspective-selector"
                             data-position="@{{ position }}"
                     >@{{ name }}</button>
