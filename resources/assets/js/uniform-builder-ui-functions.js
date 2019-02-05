97c3f340c87d244495d8bc4ab5f141c2c4704f22
Aron Joshua Bagtas <aaron@qstrike.com>
2019-02-05 16:42:20 +0800

    PTS category

--- resources/assets/js/uniform-builder-ui-functions.js
+++ resources/assets/js/uniform-builder-ui-functions.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
         var template = document.getElementById("m-richardson-right-panel-header");
         var data = {
             "uniform_name": ub.current_material.material.name,
-            "block_pattern": ub.current_material.material.block_pattern,
+            "block_pattern": ub.funcs.richardsonPTSCategory(),
             "application_type": ub.funcs.isSublimated() ? "color infused" : ub.funcs.isTackleTwill() ? "color dyed" : ""
         };
 
@@ -163,5 +163,25 @@ $(document).ready(function() {
         $("div.richardson-footer .richardson-onPrevious").css('pointer-events', 'auto');
         $(".richardson-footer .richardson-onNext").css('pointer-events', 'auto');
     };
+
+    ub.funcs.richardsonPTSCategory = function() {
+        var category;
+        var blockPattern = ub.current_material.material.name;
+
+        if (blockPattern.includes("PTS Signature")) {
+            category = "PTS 90"
+        } else if (blockPattern.includes("PTS Pro Select")) {
+            category = "PTS 80"
+        } else if (blockPattern.includes("PTS Select")) {
+            category = "PTS 70"
+        } else if (blockPattern.includes("PTS Five-Tool")) {
+            category = "PTS 10"
+        } else if (blockPattern.includes("PTS Wheelhouse")) {
+            category = "PTS 20"
+            console.log("Here")
+        }
+
+        return category;
+    }
 
 })
