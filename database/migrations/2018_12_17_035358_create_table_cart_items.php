3e8d6f158e55029d8c7cb8c746241286879723e9
rodz3rd2 <rodrigogalura3rd@gmail.com>
2019-02-21 13:33:05 +0800

    update cart_items table in database

--- database/migrations/2018_12_17_035358_create_table_cart_items.php
+++ database/migrations/2018_12_17_035358_create_table_cart_items.php
@@ -20,10 +20,8 @@ class CreateTableCartItems extends Migration
             $table->string('front_image')->nullable()->default(null);
             $table->string('back_image')->nullable()->default(null);
             $table->string('right_image')->nullable()->default(null);
+            $table->double('price')->nullable()->default(null);
             $table->smallInteger('quantity')->default(1);
-            $table->double('total_price')->nullable()->default(null);
-            $table->double('profit_amount')->nullable()->default(null);
-            $table->double('total_profit_amount')->nullable()->default(null);
             $table->string('brand', 20)->nullable()->default(null);
             $table->bigInteger('item_id')->nullable()->default(null);
             $table->bigInteger('block_pattern_id')->nullable()->default(null);
@@ -35,7 +33,6 @@ class CreateTableCartItems extends Migration
             $table->integer('factory_order_id')->nullable()->default(null);
             $table->string('design_sheet')->nullable()->default(null);
             $table->json('roster')->nullable()->default(null);
-            $table->double('price')->nullable()->default(null);
             $table->string('applicationType')->nullable()->default(null);
             $table->string('additional_attachments')->nullable()->default(null);
             $table->string('notes')->nullable()->default(null);
