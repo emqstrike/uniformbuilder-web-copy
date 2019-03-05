aa2dab4aad3c63bcc64f53381e1eb3d3b6239e6e
Aron Joshua Bagtas <aaron@qstrike.com>
2019-03-05 17:01:34 +0800

    Prolook x HSEL

--- resources/assets/js/uniform-builder-hsel.js
+++ resources/assets/js/uniform-builder-hsel.js
@@ -1,13 +1,14 @@
 $(document).ready(function($) {
     ub.funcs.initHSELLogo = function() {
         if (ub.config.styles.isFromHSEL) {
-            $("#navbar-header").append('<a class="navbar-brand btn hsel-logo" href="javascript:void(0)"><img src="/images/hsel/logo.png" class="hsel-logo"></a>');
+            $("#navbar-header").append('<a class="navbar-brand btn" style="padding: 21px 20px 20px 10px !important;"><img src="https://pngimage.net/wp-content/uploads/2018/06/x-image-png-4.png" style="height: 20px; width: 20px;"></a>');
+            $("#navbar-header").append('<a class="navbar-brand btn" id="hsel-logo" href="javascript:void(0)"><img src="/images/hsel/logo.png" class="hsel-logo"></a>');
             $(".main-picker-items[data-item='Men'").trigger("click");
             _.delay(function() {
                 $('.main-picker-items.apparel.grow[data-item="eSports"]').trigger("click");
             }, 100)
 
-            $('a.hsel-logo').on('click', function(event) {
+            $('a#hsel-logo').on('click', function(event) {
                 event.preventDefault();
                 /* Act on the event */
                 $(".main-picker-items[data-item='Men'").trigger("click");
