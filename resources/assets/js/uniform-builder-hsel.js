4b37fbdba1488ba34e2a8327fc7bd7f4c9f0a47c
Aron Joshua Bagtas <aaron@qstrike.com>
2019-03-05 14:05:50 +0800

    Change logo

--- resources/assets/js/uniform-builder-hsel.js
+++ resources/assets/js/uniform-builder-hsel.js
@@ -2,7 +2,7 @@ $(document).ready(function($) {
     ub.funcs.initHSELLogo = function() {
         if (ub.config.styles.isFromHSEL) {
             console.log("Render HSEL LOGO")
-            $("#navbar-header").append('<a class="navbar-brand btn" href="https://www.highschoolesportsleague.com/" target="_blank"><img src="/images/hsel/logo.png" height="50em" style="background: black; width: 108px;"></a>');
+            $("#navbar-header").append('<a class="navbar-brand btn" href="https://www.highschoolesportsleague.com/" target="_blank"><img src="/images/hsel/logo.png" class="hsel-logo"></a>');
             $(".main-picker-items[data-item='Men'").trigger("click");
             _.delay(function() {
                 $('.main-picker-items.apparel.grow[data-item="eSports"]').trigger("click");
