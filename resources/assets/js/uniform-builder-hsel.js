bee661e0726534b31a9fab24eb61aea6d08d8629
Aron Joshua Bagtas <aaron@qstrike.com>
2019-03-06 09:27:48 +0800

    Remove min-width in logo btn

--- resources/assets/js/uniform-builder-hsel.js
+++ resources/assets/js/uniform-builder-hsel.js
@@ -3,6 +3,8 @@ $(document).ready(function($) {
         if (ub.config.styles.isFromHSEL) {
             $("#navbar-header").append('<a class="navbar-brand btn" style="padding: 21px 20px 20px 10px !important;"><img src="https://pngimage.net/wp-content/uploads/2018/06/x-image-png-4.png" style="height: 20px; width: 20px;"></a>');
             $("#navbar-header").append('<a class="navbar-brand btn" id="hsel-logo" href="javascript:void(0)"><img src="/images/hsel/logo.png" class="hsel-logo"></a>');
+            $(".navbar-brand.btn.dropdown-toggle").css('min-width', '0');
+
             $(".main-picker-items[data-item='Men'").trigger("click");
             _.delay(function() {
                 $('.main-picker-items.apparel.grow[data-item="eSports"]').trigger("click");
