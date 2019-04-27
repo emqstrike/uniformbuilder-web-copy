8022cd4d9aa1c72a47e244fb169568673a7c7e3f
AronJoshua <aaron@qstrike.com>
2019-04-27 11:22:47 +0800

    richardson

--- resources/views/r-uniform-builder.blade.php
+++ resources/views/r-uniform-builder.blade.php
@@ -14,9 +14,13 @@
     <meta name="description" content="Design your own custom uniforms using the Prolook Uniform Customizer. We offer tons of designs for all sports. Create your custom uniform today.">
     <meta name="keywords" content="custom uniform, custom football uniform, custom basketball uniform, custom baseball uniform, custom volleyball uniform, uniform builder, prolook unifom builder, team uniforms">
     <link rel="icon" type="image/png" href="/images/branding/favicon.ico"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.42/css/uikit.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/roundSlider/1.3.2/roundslider.min.css" />
+    <link rel="stylesheet" href="https://simeydotme.github.io/jQuery-ui-Slider-Pips/dist/css/jqueryui.min.css" />
+    <link rel="stylesheet" href="https://rawgit.com/simeydotme/jQuery-ui-Slider-Pips/master/dist/jquery-ui-slider-pips.css" />
+    <link rel="stylesheet" href="{{$asset_storage}}/richardson/css/richardson-builder.css?v={{$asset_version}}">
 
     @include('partials.code.rollbar-js')
-    @include('partials.code.stylesheets')
 
     <script type="text/javascript" src='https://www.google.com/recaptcha/api.js'></script>
 </head>
@@ -26,7 +30,7 @@
     @include('partials.panels.debug-panel')
     <!-- End Preview Panel -->
 </div>
-<section class="uk-section">
+<section class="uk-section" id="main-container">
     <div class="uk-container uk-container-large">
         <div class="uk-grid-collapse" uk-grid>
             <div class="uk-width-1-1 uk-width-1-2@m uk-width-2-3@l ">
@@ -36,11 +40,8 @@
                             <div class="con-preview uk-width-1-1 uk-width-3-4@s">
                                 <div class="uk-inline">
                                     <!-- Main Canvas -->
-                                    <div class="canvas-views" id="main_view">
-                                        <div class="verbiage-container">
-                                            <p class="verbiage-text">
-                                                &quot;The finished product color may vary slightly from image shown on your monitor. This may be caused by brightness, contrast or other monitor settings.&quot;
-                                            </p>
+                                    <div class="uk-cover-container" uk-height-viewport>
+                                        <div class="canvas-views" id="main_view">
                                         </div>
                                     </div>
 
@@ -95,43 +96,73 @@
                 <div class="uk-grid-collapse" uk-grid>
                     <!-- Number Navigation -->
                     <div class="uk-width-1-1 uk-width-auto@s left-nav">
-                        <div class="uk-flex-center uk-grid-collapse uk-child-width-auto uk-text-right@m active-bgc-red active-bdr-red uk-text-bold" uk-switcher="connect: .con-customizer" uk-grid>
+                        <div class="uk-flex-center uk-grid-collapse uk-child-width-auto uk-text-right@m active-bgc-red active-bdr-red uk-text-bold" id="property-modifiers-menu" uk-grid>
                             <div class="uk-active">
-                                <a href="" class="uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-hidden@s" uk-tooltip="title: Fabric; pos: top-right">1</a>
-                                <a href="" class="uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-visible@s" uk-tooltip="title: Fabric; pos: left; ">1</a>
+                                <!-- <a href="" class="group-pane abrade-ultra menu-item group-1 menu-item-fabrics uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-hidden@s" uk-tooltip="title: Fabric; pos: top-right">1</a> -->
+                                <a href="" class="group-pane abrade-ultra menu-item group-1 menu-item-fabrics uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-visible@s" uk-tooltip="title: Fabric; pos: left; ">1</a>
                             </div>
                             <div class="">
-                                <a href="" class="uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-hidden@s" uk-tooltip="title: Base Color; pos: top-right">2</a>
-                                <a href="" class="uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-visible@s" uk-tooltip="title: Base Color; pos: left; ">2</a>
+                                <!-- <a href="" class="group-pane abrade-ultra menu-item group-2 menu-item-parts uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-hidden@s" uk-tooltip="title: Base Color; pos: top-right">2</a> -->
+                                <a href="" class="group-pane abrade-ultra menu-item group-2 menu-item-parts uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-visible@s" uk-tooltip="title: Base Color; pos: left; ">2</a>
                             </div>
                             <div class="">
-                                <a href="" class="uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-hidden@s" uk-tooltip="title: Inserts; pos: top-right">3</a>
-                                <a href="" class="uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-visible@s" uk-tooltip="title: Inserts; pos: left; ">3</a>
+                                <!-- <a href="" class="group-pane abrade-ultra menu-item group-3 menu-item-piping uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-hidden@s" uk-tooltip="title: Inserts; pos: top-right">3</a> -->
+                                <a href="" class="group-pane abrade-ultra menu-item group-3 menu-item-piping uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-visible@s" uk-tooltip="title: Inserts; pos: left; ">3</a>
                             </div>
                             <div class="">
-                                <a href="" class="uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-hidden@s" uk-tooltip="title: Pipings; pos: top-right">4</a>
-                                <a href="" class="uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-visible@s" uk-tooltip="title: Pipings; pos: left">4</a>
+                                <!-- <a href="" class="group-pane abrade-ultra menu-item group-4 menu-item-letters uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-hidden@s" uk-tooltip="title: Pipings; pos: top-right">4</a> -->
+                                <a href="" class="group-pane abrade-ultra menu-item group-4 menu-item-letters uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-visible@s" uk-tooltip="title: Pipings; pos: left">4</a>
                             </div>
                             <div class="">
-                                <a href="" class="uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-hidden@s" uk-tooltip="title: Decoration Letters; pos: top">5</a>
-                                <a href="" class="uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-visible@s" uk-tooltip="title: Decoration Letters; pos: left">5</a>
+                                <!-- <a href="" class="group-pane abrade-ultra menu-item group-5 menu-item-numbers uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-hidden@s" uk-tooltip="title: Decoration Letters; pos: top">5</a> -->
+                                <a href="" class="group-pane abrade-ultra menu-item group-5 menu-item-numbers uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-visible@s" uk-tooltip="title: Decoration Letters; pos: left">5</a>
                             </div>
                             <div class="">
-                                <a href="" class="uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-hidden@s" uk-tooltip="title: Decoration Numbers; pos: top">6</a>
-                                <a href="" class="uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-visible@s" uk-tooltip="title: Decoration Numbers; pos: left">6</a>
+                                <!-- <a href="" class="group-pane abrade-ultra menu-item group-6 menu-item-applications uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-hidden@s" uk-tooltip="title: Decoration Numbers; pos: top">6</a> -->
+                                <a href="" class="group-pane abrade-ultra menu-item group-6 menu-item-applications uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-visible@s" uk-tooltip="title: Decoration Numbers; pos: left">6</a>
                             </div>
                             <div class="">
-                                <a href="" class="uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-hidden@s" uk-tooltip="title: Decorations; pos: top">7</a>
-                                <a href="" class="uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-visible@s" uk-tooltip="title: Decorations; pos: left">7</a>
-                            </div>
-                            <div class="">
-                                <a href="" class="uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-hidden@s" uk-tooltip="title: Richardson Logo; pos: top">8</a>
-                                <a href="" class="uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-visible@s" uk-tooltip="title: Richardson Logo; pos: left">8</a>
+                                <!-- <a href="" class="group-pane abrade-ultra menu-item group-7 menu-item-logo uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-hidden@s" uk-tooltip="title: Decorations; pos: top">7</a> -->
+                                <a href="" class="group-pane abrade-ultra menu-item group-7 menu-item-logo uk-icon-button bdrr-none bdr-thin bdr-light uk-link-reset uk-visible@s" uk-tooltip="title: Decorations; pos: left">7</a>
                             </div>
                         </div>
                     </div>
                     <!-- End Number Navigation -->
-                    <div class="uk-width-1-1 uk-width-expand@s">
+                    <div class="uk-width-1-1 uk-width-expand@s pane-main-window" id="right-main-window">
+                        @include('partials.panels.materials')
+
+                        @include('partials.panels.colors')
+
+                        @include('partials.panels.patterns')
+
+                        @include('partials.panels.gradients')
+
+                        @include('partials.panels.applications')
+
+                        @include('partials.panels.texts')
+
+                        @include('partials.panels.numbers')
+
+                        @include('partials.panels.graphics')
+
+                        @include('partials.panels.sizes')
+
+                        @include('partials.panels.attachments')
+
+                        @include('partials.panels.mod_main')
+
+                        @include('partials.panels.mod_primary')
+
+                        @include('partials.panels.layers')
+
+                        @include('partials.panels.pipings')
+
+                        @include('partials.panels.randomFeeds')
+
+                        <!-- Richardson Modals file -->
+                        @if(env("BRAND") === "Richardson")
+                            @include('partials.panels.richardson-application-modal')
+                        @endif
                     </div>
                 </div>
             </div>
@@ -139,12 +170,32 @@
     </div>
 </section>
 
+
+    <!-- UI Controls --> 
+
+    <!-- Prolook / Base -->
+    @include('partials.controls.ui-controls')
+
+    <!-- Richardson UI Controls -->
+    @include('partials.controls.ui-controls-richardson')
+
+    <!-- End UI Controls -->
+
+
+    <!-- Code -->
+
+    <!-- Third Party Scripts -->
+    @include('partials.code.includes')
+
+    <!-- Init Code -->
+    @include('partials.code.init-code')
+    <!-- End Code -->
+
+
+
 <script type="text/javascript">
     window.brand_env = '{{ env('BRAND') }}';
-
-    if (brand_env === "Richardson") {
-        document.getElementById("main_container").style.display = "none";
-    }
 </script>
+<script src="{{$asset_storage}}/uniform-builder/js/ub.js?v={{$asset_version}}"></script>
 </body>
 </html>
