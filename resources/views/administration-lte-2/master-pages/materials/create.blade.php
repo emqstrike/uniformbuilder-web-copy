8ba2c1d1136b35c5fef59f4fab88adadb095d425
ayellutero <ayellutero@gmail.com>
2019-05-03 14:26:20 +0800

    initial commit; add new fields in add/edit material V1

--- resources/views/administration-lte-2/master-pages/materials/create.blade.php
+++ resources/views/administration-lte-2/master-pages/materials/create.blade.php
@@ -464,6 +464,20 @@
                                     </select>
                                 </div>
                             </div>
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Model Name</label>
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control model-name"  name="model_name" >
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Style Number</label>
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control style-number"  name="style_number" >
+                                </div>
+                            </div>
+                            
                             <div class="form-group">
                                 <div class="col-md-6 col-md-offset-4">
                                     <button type="submit" class="btn btn-primary create-user">
