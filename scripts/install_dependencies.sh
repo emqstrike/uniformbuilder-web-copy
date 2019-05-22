2d03d46f8d899c29d812404e6922ffdfd515c234
Rohmer <rohmer@qstrike.com>
2019-05-22 11:46:18 +0800

    update package

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sudo apt-get install php7.2 php7.2-cli php7.2-curl php7.2-fpm php7.2-gd php7.2-json 
-php7.2-mbstring php7.2-mcrypt php7.2-mysql php7.2-soap php7.2-sqlite3 php7.2-xml php7.2-zip
+echo "Hello world"
