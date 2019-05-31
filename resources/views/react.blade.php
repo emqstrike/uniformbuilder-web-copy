4646f7aa1ae0ee580f56643b6651a3ad4f2d19d0
Nico Sevilla <nico@qstrike.com>
2019-05-31 18:18:35 +0800

    Edit view file

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>REACT APP</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <link rel="shortcut icon" href="/images/branding/favicon.ico" />
+    <title>React App</title>
 </head>
-
 <body>
+<noscript>You need to enable JavaScript to run this app.</noscript>
 <div id="root"></div>
 <script src="{{$asset_storage}}/uniform-builder/dist/app.js?v={{$asset_version}}"></script>
 </body>
-
 </html>
