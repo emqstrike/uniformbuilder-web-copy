7b548444bbca0bdf3c5985d22a487aa202625435
ayellutero <ayellutero@gmail.com>
2019-07-23 15:47:24 +0800

    Initial qx style request

---
+++ app/Http/Controllers/AdministrationV2/Qx7StyleRequestController.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Http\Controllers\AdministrationV2;
+
+use \Session;
+use \Redirect;
+use App\Http\Requests;
+use App\Utilities\Log;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class Qx7StyleRequestController extends Controller
+{
+
+    public function __construct(
+        // APIClient $apiClient
+    )
+    {
+        // $this->client = $apiClient;
+    }
+
+    public function index()
+    {
+
+        return view('administration-lte-2.qx7-style-requests.view');
+
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -258,6 +258,9 @@ Route::group(array('prefix' => 'administration', 'middleware' => 'disablePrevent
 
             // Page Rules
             Route::get('page_rules', ['middleware' => 'adminAccess', 'uses' => 'AdministrationV2\PageRuleController@index'])->name('v1_page_rules');
+
+            // QX7 Style Requests
+            Route::get('qx7_style_requests', ['middleware' => 'adminAccess', 'uses' => 'AdministrationV2\Qx7StyleRequestController@index'])->name('v1_qx7_style_requests');
         });
     });
 
