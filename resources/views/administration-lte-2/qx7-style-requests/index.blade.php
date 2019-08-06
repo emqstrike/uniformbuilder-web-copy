6efc63d13b9b46cd6fd70af787d363236ea28514
unknown <angel@qstrike.com>
2019-08-06 13:31:04 +0800

    updated style id

--- resources/views/administration-lte-2/qx7-style-requests/index.blade.php
+++ resources/views/administration-lte-2/qx7-style-requests/index.blade.php
@@ -88,13 +88,13 @@ $(document).ready(function(){
                             elem += `<a href="/administration/v1-0/qx7_style_requests/create_style/` + request.id + `" class="btn btn-default btn-xs" data-style-id="` + request.id + `" role="button">Create Style</a>`
                         } else {
                             elem += `<a href="/administration/v1-0/qx7_style_requests/edit/`+ request.id + `" class="btn btn-default btn-xs" data-style-id="` + request.id + `" role="button">Update Style</a>
-                                    <a href="/administration/v1-0/qx7_style_requests/view_options/`+ request.id + `" class="btn btn-xs btn-flat btn-default">
+                                    <a href="/administration/v1-0/qx7_style_requests/view_options/`+ request.style_id + `" class="btn btn-xs btn-flat btn-default">
                                         Options
                                     </a>
-                                    <a href="/administration/v1-0/qx7_style_requests/view_options_setup/`+ request.id + `" class="btn btn-xs btn-flat btn-default">
+                                    <a href="/administration/v1-0/qx7_style_requests/view_options_setup/`+ request.style_id + `" class="btn btn-xs btn-flat btn-default">
                                         <i class="fa fa-gear" aria-hidden="true"></i>
                                     </a>
-                                    <a href="/administration/v1-0/qx7_style_requests/options/dropzone/`+ request.id + `" class="btn btn-xs btn-flat btn-default">
+                                    <a href="/administration/v1-0/qx7_style_requests/options/dropzone/`+ request.style_id + `" class="btn btn-xs btn-flat btn-default">
                                         <i class="fa fa-upload" aria-hidden="true"></i>
                                     </a>`
                         }
