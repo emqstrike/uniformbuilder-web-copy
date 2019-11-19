3de170b058effcceaec261891203b7b49f07b17f
John Jay <jj22511@gmail.com>
2019-11-19 14:40:48 +0800

    integrated zoho live chat

--- resources/views/uniform-builder.blade.php
+++ resources/views/uniform-builder.blade.php
@@ -142,28 +142,7 @@
 
     @include('partials.detect-mobile')
 
-    {{-- Zoho Live Chat --}}
-    <script>
-        var $zoho = $zoho || {};
-        $zoho.salesiq = $zoho.salesiq || {
-            widgetcode:"b122af8e0746322084e888cc4eb0979a67c851fc227c5e73271feec78d0c9736",
-            values:{},
-            ready:function(){}
-        };
-
-        (function() {
-            var d = document;
-            s = d.createElement("script");
-            s.type = "text/javascript";
-            s.id = "zsiqscript";
-            s.defer = true;
-            s.src = "https://salesiq.zoho.com/widget";
-            t = d.getElementsByTagName("script")[0];
-            t.parentNode.insertBefore(s,t);
-            d.write("<div id='zsiqwidget'></div>");
-        })();
-    </script>
-    {{-- /Zoho Live Chat  --}}
+    @include('partials.live-chat')
 
 </body>
 </html>
