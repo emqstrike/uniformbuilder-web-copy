4de84ceb3baba27adf6566324119a7d02e52f4ac
unknown <angel@qstrike.com>
2019-11-28 14:08:11 +0800

    refactor libs

--- resources/views/administration-lte-2/lte-main.blade.php
+++ resources/views/administration-lte-2/lte-main.blade.php
@@ -8,20 +8,26 @@
         <title>Administration | {{ env('APP_TITLE') }}</title>
 
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+        <link rel="stylesheet" href="/admin-lte-2/bootstrap/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="/admin-lte-2/datatables/css/dataTables.bootstrap.min.css">
         <link rel="stylesheet" href="/admin-lte-2/font-awesome/css/font-awesome.min.css">
         <link rel="stylesheet" href="/admin-lte-2/Ionicons/css/ionicons.min.css">
+
         <link rel="stylesheet" href="/admin-lte-2/dist/css/AdminLTE.min.css">
         <link rel="stylesheet" href="/admin-lte-2/dist/css/skins/_all-skins.min.css">
+
+        <link rel="stylesheet" href="/plugins/datepicker/datepicker3.css">
+        <link rel="stylesheet" href="/plugins/daterangepicker/daterangepicker-bs3.css">
         <link rel="stylesheet" type="text/css" href="/admin-lte-2/css/libs/pnotify/pnotify.custom.min.css">
         <link rel="stylesheet" type="text/css" href="/admin-lte-2/css/libs/spectrum/spectrum.css">
         <link rel="stylesheet" type="text/css" href="/admin-lte-2/css/libs/select2/select2.min.css">
-        <link rel="stylesheet" href="/plugins/datepicker/datepicker3.css">
-        <link rel="stylesheet" href="/plugins/daterangepicker/daterangepicker-bs3.css">
+        <link rel="stylesheet" type="text/css" href="/css/custom.css">
+
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
         <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons" rel="stylesheet">
         <link rel="stylesheet" href="https://unpkg.com/vue-multiselect@2.1.0/dist/vue-multiselect.min.css">
-        <link rel="stylesheet" href="/admin-lte-2/bootstrap/dist/css/bootstrap.min.css">
+
 
         <style>
             #breadcrumbs {
@@ -81,21 +87,29 @@
 
         <script type="text/javascript" src="/admin-lte-2/jquery/dist/jquery-3.3.1.min.js"></script>
         <script type="text/javascript" src="/admin-lte-2/jquery-ui/dist/jquery-ui.min.js"></script>
-        <script type="text/javascript" src="/admin-lte-2/datatables/js/jquery.dataTables.min.js"></script>
-        <script type="text/javascript" src="/admin-lte-2/datatables/js/dataTables.bootstrap.min.js"></script>
+
         <script src="/admin-lte-2/bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="/plugins/daterangepicker/daterangepicker.js"></script>
+        <script src="/plugins/datepicker/bootstrap-datepicker.js"></script>
         <script src="/admin-lte-2/jquery-slimscroll/jquery.slimscroll.min.js"></script>
         <script src="/admin-lte-2/fastclick/lib/fastclick.js"></script>
+
         <script src="/admin-lte-2/dist/js/adminlte.min.js"></script>
+
+        <script type="text/javascript" src="/admin-lte-2/datatables/js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript" src="/admin-lte-2/datatables/js/dataTables.bootstrap.min.js"></script>
+        <script type="text/javascript" src="/js/libs/bootstrap-table/bootstrap-table.min.js"></script>
         <script type="text/javascript" src="/js/bootbox.min.js"></script>
         <script type="text/javascript" src="/admin-lte-2/js/libs/pnotify/pnotify.custom.min.js"></script>
         <script type="text/javascript" src="/admin-lte-2/js/libs/spectrum/spectrum.js"></script>
+        <script type="text/javascript" src="/js/ddslick.min.js"></script>
         <script type="text/javascript" src="/admin-lte-2/js/libs/underscore/underscore-min.js"></script>
         <script type="text/javascript" src="/admin-lte-2/js/libs/select2/select2.min.js"></script>
-        <script src="https://unpkg.com/vue-multiselect@2.1.0"></script>
+        <script type="text/javascript" src="/fabricjs/fabric.min.js"></script>
+        <script type="text/javascript" src="/fabricjs/customiseControls.js"></script>
         <script type="text/javascript" src="/js/administration/common.js"></script>
-        <script src="/plugins/daterangepicker/daterangepicker.js"></script>
-        <script src="/plugins/datepicker/bootstrap-datepicker.js"></script>
+
+        <script src="https://unpkg.com/vue-multiselect@2.1.0"></script>
         <script src="/js/libs/axios.js"></script>
         <script src="/js/libs/axios-global-config.js"></script>
         <script src="https://unpkg.com/vue@2.5.18/dist/vue.js"></script>
