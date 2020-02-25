55953590301238b7fc68eb809bc77c27a39a61a0
iamjowin <jowin@qstrike.com>
2020-02-25 11:46:21 +0800

    fixing changes of total entries on save design after deleting

--- resources/assets/js/uniform-builder.js
+++ resources/assets/js/uniform-builder.js
@@ -8933,6 +8933,7 @@ $(document).ready(function () {
 
                 success: function (response) {
                     $('tr.saved-design-row[data-id="' + id + '"]').fadeOut();
+                    location.reload();
                 }
             });
         }
